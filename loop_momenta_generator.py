639a3ccd00a00b4a1b960b770dac284671e4d039
Ben Ruijl <benruyl@gmail.com>
2018-07-03 12:10:50 +0200

    Added function to find the P+/P- vectors

--- loop_momenta_generator.py
+++ loop_momenta_generator.py
@@ -14,9 +14,11 @@ from madgraph import InvalidCmd, MadGraph5Error
 
 logger = logging.getLogger('pyNLoop.LoopMomentaGenerator')
 
+
 class LoopMomentaGeneratorError(MadGraph5Error):
     """ Error for the LoopMomentaGenerator class suite."""
     pass
+
 
 class LoopMomentaGenerator(object):
     """ Class for recursively using OneLoopMomentumGenerator for generaring several complex-valued loop momenta
@@ -38,16 +40,16 @@ class OneLoopMomentumGenerator(LoopMomentaGenerator):
 
         self.external_momenta = external_momenta
         # Eventually this information must be extracted from the topology, for now it is hard-coded.
-        self.loop_propagator_masses = [0.,]*4
+        self.loop_propagator_masses = [0., ]*4
 
     def generate_loop_momenta(self, random_variables):
         """ From the random variables passed in argument, this the deform one-loop four-momentum in the form
          of a LorentzVector."""
 
-        if len(random_variables)!=4 or any((r>1. or r<0.) for r in random_variables):
+        if len(random_variables) != 4 or any((r > 1. or r < 0.) for r in random_variables):
             raise LoopMomentaGeneratorError(
-                "The function 'generate_loop_momenta' class '%s' requires exactly 4"%self.__class__.__name__+
-                                                                                 " input random variables in [0., 1.].")
+                "The function 'generate_loop_momenta' class '%s' requires exactly 4" % self.__class__.__name__ +
+                " input random variables in [0., 1.].")
         rv = random_variables
         # Return some dummy momentum for now for test purposes
         return vectors.LorentzVector([
@@ -61,3 +63,41 @@ class OneLoopMomentumGenerator(LoopMomentaGenerator):
         """ Validation function that tests that the deformation yields a complex part of each denominator with the right
         sign when approaching the point where this denominator becomes onshell."""
         pass
+
+    def findP(self, qs, plus=True):
+        """Find a vector P such that all external momenta `qs` are in
+        the forward lightcone of P if `plus`, or are in the backwards lightcone
+        if not `plus`.
+        """
+        vecs = [v for v in qs]
+        while len(vecs) > 1:
+            # filter all vectors that are in the forward/backward light-cone of another vector
+            newvec = []
+            for i, v in enumerate(vecs):
+                for v1 in vecs[i + 1:]:
+                    if (v-v1).square() < 0 and ((plus and v[0] > v1[0]) or (not plus and v[0] < v1[0])):
+                        break
+                else:
+                    # this vector is space-like relative to all others
+                    newvec.append(v)
+
+            # find the pair with the smallest space-like seperation
+            space_sep = [(i, j, -(newvec[i]-newvec[j]).square()) for i in range(newvec)
+                         for j in range(i+1, newvec)]
+            smallest = min(space_sep, key=lambda x: x[2])
+
+            def Z(x, y):
+                n = 1.0 / y.rho2()
+                if plus:
+                    return 0.5 * vectors.LorentzVector([x[0] + n*y.square() - n*y[0]**2,
+                                                        x[1] - n*y[0]*y[1], x[2] - n*y[0]*y[2], x[3] - n*y[0]*y[3]])
+                else:
+                    return 0.5 * vectors.LorentzVector([x[0] - n*y.square() + n*y[0]**2,
+                                                        x[1] + n*y[0]*y[1], x[2] + n*y[0]*y[2], x[3] + n*y[0]*y[3]])
+
+            # replace first vector and drop the second
+            newvec[smallest[0]] = Z(
+                newvec[smallest[0]] + newvec[smallest[1]], newvec[smallest[0]] - newvec[smallest[1]])
+            del newvec[smallest[1]]
+            vecs = newvec
+        return vecs[0]
