Fixed computation of qs for cycles
- Fixed lambda calculation
- Added test function

twoloop.py
@@ -54,8 +54,9 @@ class DoubleBox(integrands.VirtualIntegrand):
 
         self.parameterization = None
         self.deformation = None
-        self.interal_masses = [0., 0., 0., 0., 0., 0., 0.]
+        self.internal_masses = [0., 0., 0., 0., 0., 0., 0.]
 
+        #self.integrand = DummyIntegrand()
         self.integrand = integrand.Integrand(
             "box2L_direct_integration", channel=self.channel, region=0, mu_sq=self.mu_sq)
 
@@ -67,12 +68,13 @@ class DoubleBox(integrands.VirtualIntegrand):
         k_mapped = vectors.LorentzVector(loop_momenta[:4])
         l_mapped = vectors.LorentzVector(loop_momenta[4:])
 
-        # for the shift of 1/l^2
-        a = l_mapped - k_mapped
-
-        # cycle 12 with l -> k - l
-        C12_qs = [vectors.LorentzVector(), a, a + self.external_momenta[1] + self.external_momenta[2],
-                  a + self.external_momenta[1] + self.external_momenta[2] - self.external_momenta[0]]
+        # compute the external momenta for the C12(k) cycle and express the qs in terms of that.
+        # This is not a shift. We also set q[0] = 0 at the 1/k^2 line
+        # k, l, k - l, k - l - p2, k - l - p2 - p3, k - p2 - p3, k + p1
+        # TODO: check
+        C12_qs = [vectors.LorentzVector(), k_mapped - l_mapped,
+                  self.external_momenta[1] + self.external_momenta[2],
+                  -self.external_momenta[0]]
 
         self.deformation.set_qs([[x for x in e] for e in C12_qs])
         mapped_r, jac_real, jac_imag = self.deformation.deform(
@@ -80,16 +82,29 @@ class DoubleBox(integrands.VirtualIntegrand):
         # just take the imaginary part
         C12_k = [imag for real, imag in mapped_r]
 
-        # cycle 23 with k = k + 2l
-        C23_qs = [vectors.LorentzVector(), -k_mapped, -k_mapped + self.external_momenta[1],
-                  -k_mapped + self.external_momenta[1] + self.external_momenta[2]]
+        # now do the same for C12(l)_qs
+        # TODO: check
+        C12_qs = [vectors.LorentzVector(), -k_mapped + l_mapped + self.external_momenta[1] + self.external_momenta[2],
+                  -k_mapped + l_mapped - self.external_momenta[0],
+                  -k_mapped + l_mapped]
+
+        self.deformation.set_qs([[x for x in e] for e in C12_qs])
+        mapped_r, jac_real, jac_imag = self.deformation.deform(
+            [x for x in l_mapped])
+        # just take the imaginary part
+        C12_l = [imag for real, imag in mapped_r]
+
+        # cycle C23(l), going with the momentum direction
+        # TODO: check
+        C23_qs = [vectors.LorentzVector(), -k_mapped + self.external_momenta[1] + self.external_momenta[2],
+                  -k_mapped + self.external_momenta[1], -k_mapped]
 
         self.deformation.set_qs([[x for x in e] for e in C23_qs])
         mapped_r, jac_real, jac_imag = self.deformation.deform(
             [x for x in l_mapped])
-        C23_k = [imag for real, imag in mapped_r]
+        C23_l = [imag for real, imag in mapped_r]
 
-        # cycle 13 without shift
+        # cycle C13(k)
         C13_qs = [vectors.LorentzVector(), l_mapped, l_mapped + self.external_momenta[1],
                   l_mapped +
                   self.external_momenta[1] + self.external_momenta[2],
@@ -102,21 +117,24 @@ class DoubleBox(integrands.VirtualIntegrand):
         C13_k = [imag for real, imag in mapped_r]
 
         k_1 = vectors.LorentzVector(C12_k) + vectors.LorentzVector(C13_k)
-
-        # TODO: what about all the shifts?
-        # should we use C12_l instead?
-        k_2 = vectors.LorentzVector(C12_k) + vectors.LorentzVector(C23_k)
+        k_2 = vectors.LorentzVector(C12_l) + vectors.LorentzVector(C23_l)
 
         lambda_overall = 1  # maximum lambda value
-        ks = [k_1, k_2]
-        lmom = [k_mapped, l_mapped]
 
-        # TODO: the paper suggests the loop is over the propagators
-        # but we only have two deformed ks instead of 7 and their propagator is simply 1/k^2
-        for j in range(2):
-            xj = (ks[j].dot(lmom[j]) / ks[j].square())**2
+        # propagators with substituted momenta split in real and imag
+        # k, l, k - l, k - l - p2, k - l - p2 - p3, k - p2 - p3, k + p1
+        props = [(k_mapped, k_1), (l_mapped, k_2), (k_mapped - l_mapped, k_1 - k_2),
+                 (k_mapped - l_mapped - self.external_momenta[1], k_1 - k_2),
+                 (k_mapped - l_mapped -
+                  self.external_momenta[1] - self.external_momenta[2], k_1 - k_2),
+                 (k_mapped - self.external_momenta[1] -
+                  self.external_momenta[2], k_1),
+                 (k_mapped + self.external_momenta[0], k_1)]
+
+        for (qt, kt), m in zip(props, self.internal_masses):
+            xj = (kt.dot(qt) / kt.square())**2
             yj = (
-                (lmom[j]).square() - self.interal_masses[j]**2) / ks[j].square()
+                qt.square() - m**2) / kt.square()
 
             if 2 * xj < yj:
                 lambda_overall = min(lambda_overall, sqrt(yj / 4.))
@@ -136,26 +154,30 @@ class DoubleBox(integrands.VirtualIntegrand):
         self.parameterization.set_mode("log")
         k_mapped, jac_k = self.parameterization.map(k)
         l_mapped, jac_l = self.parameterization.map(l)
-
-        ks = self.deform(k_mapped + l_mapped)
-
-        # compute numerical Jacobian
-        def wrapped_function(loop_momenta):
-            return np.r_[self.deform(loop_momenta)]
-
         numerical_jacobian_weight = 1.
-        local_point = k_mapped + l_mapped
+        ks = k_mapped + l_mapped
 
-        numerical_jacobian, info = nd.Jacobian(
-            wrapped_function, full_output=True)(local_point)
+        if True:
+            ks = self.deform(k_mapped + l_mapped)
 
-        # And now compute the determinant
-        numerical_jacobian_weight = linalg.det(numerical_jacobian)
+            # compute numerical Jacobian
+            def wrapped_function(loop_momenta):
+                return np.r_[self.deform(loop_momenta)]
 
-        if np.max(info.error_estimate) > 1.e-3:
-            print(
-                "Large error of %f (for which det(jac)=%s) encountered in the numerical evaluation of the Jacobian for the inputs: %s" %
-                (np.max(info.error_estimate), numerical_jacobian_weight, str(local_point)))
+            local_point = k_mapped + l_mapped
+
+            # note: method=forward reduces the number of samples from 244 to 122
+            numerical_jacobian = nd.Jacobian(
+                wrapped_function, full_output=False, method='central')(local_point)
+
+            # And now compute the determinant
+            numerical_jacobian_weight = linalg.det(numerical_jacobian)
+
+            # NOTE: This happens a lot!
+            # if np.max(info.error_estimate) > 1.e-3:
+            #    print(
+            #        "Large error of %f (for which det(jac)=%s) encountered in the numerical evaluation of the Jacobian for the inputs: %s" %
+            #        (np.max(info.error_estimate), numerical_jacobian_weight, str(local_point)))
 
         # integrate the two-loop
         out_real, out_imag = self.integrand.evaluate([ks[:4], ks[4:]])
@@ -184,22 +206,26 @@ class DoubleBox(integrands.VirtualIntegrand):
 
         self.external_momenta = [random_PS_point[x] for x in range(1, 5)]
 
+        print("Analytic result: %s" %
+              self.doublebox_analytic(*self.external_momenta))
+
         # defaults to Weinzierl mapping
         # set dummy qs
         self.parameterization = deformation.Parameterization(
             e_cm_sq=sqrt_s**2, region=0, channel=self.channel, qs_py=[[v for v in q] for q in self.external_momenta])
 
         self.deformation = deformation.Deformation(
-            e_cm_sq=sqrt_s**2, mu_sq=self.mu_sq, region=0, qs_py=[[v for v in q] for q in self.external_momenta], masses=self.interal_masses)
+            e_cm_sq=sqrt_s**2, mu_sq=self.mu_sq, region=0, qs_py=[[v for v in q] for q in self.external_momenta], masses=self.internal_masses)
 
         self.integrand.set_externals([[x for x in e]
                                       for e in self.external_momenta])
 
         # TODO: set sensible options
-        integrator = Vegas3Integrator(self, verbosity=1, cluster=MultiCore(4), survey_n_points=10000, survey_n_iterations=10, refine_n_points=20000, refine_n_iterations=5)
+        integrator = Vegas3Integrator(self, verbosity=1, cluster=MultiCore(
+            4), survey_n_points=10000, survey_n_iterations=10, refine_n_points=20000, refine_n_iterations=5)
 
         amplitude, error = integrator.integrate()
-        print("Result: %s +/ %s", amplitude, error)
+        print("Result: %s +/ %s" % (amplitude, error))
 
     def sample_points(self):
         """Sample random points"""
@@ -226,7 +252,7 @@ class DoubleBox(integrands.VirtualIntegrand):
                                       for e in external_momenta])
 
         t = time.time()
-        N = 10
+        N = 30
 
         random.seed(123)
 
@@ -251,22 +277,42 @@ class DoubleBox(integrands.VirtualIntegrand):
         def rho(x, y):
             return 2. / (1. - x - y + lmbda(x, y))
 
-        s = (k1 + k2)**2
-        t = (k2 + k3)**2
+        s = (k1 + k2).square()
+        t = (k2 + k3).square()
 
-        x = k1**2 * k3**2 / float(s * t)
-        y = k2**2 * k4**2 / float(s * t)
-
-        if x < 0 or y < 0:
-            print("Warning: analytical contituation of ln needs to be performed!")
+        x = k1.square() * k3.square() / float(s * t)
+        y = k2.square() * k4.square() / float(s * t)
 
         l = lmbda(x, y)
         r = rho(x, y)
         rx = r * x
         ry = r * y
 
+        ln_rx = ln(rx) if x >= 0 else ln(-rx) + 1j * pi * np.sign(s*t)
+        ln_ry = ln(ry) if y >= 0 else ln(-ry) + 1j * pi * np.sign(s*t)
+
         return - t * (pi**2*1j/(s*t))**2 / l * (6. * (polylog(4, -rx) + polylog(4, -ry)) + 3 * ln(y/x)*(polylog(3, -rx) - polylog(3, -ry)) + 0.5*ln(y/x)**2*(polylog(2, -rx)+polylog(2, -ry))
-                                                + 0.25*ln(rx)**2*ln(ry)**2 + pi**2/2.*ln(rx)*ln(ry) + pi**2/12*ln(y/x)**2 + 7 * pi**4/60.)
+                                                + 0.25*ln_rx**2*ln_ry**2 + pi**2/2.*ln_rx*ln_ry + pi**2/12*ln(y/x)**2 + 7 * pi**4/60.)
+
+
+class DummyIntegrand():
+    def set_externals(self, *args):
+        pass
+
+    def evaluate(self, l_moms):
+        euclidian_product = sum(
+            l_moms[0][i] ** 2 for i in range(4)) + sum(l_moms[1][i] ** 2 for i in range(4))
+        Mass_scale = 1.
+        regulator = 50.
+        d = 6
+        #
+        # with the above parameter, the analytical result then reads for:
+        #  Mass_scale = 1.   result --> 2.6654
+        #  Mass_scale = 100. result --> 2.6087*10^16
+        #  mscale=1, reg=500 result --> 1.23639
+        integrand = (
+            1. / ((euclidian_product / (Mass_scale ** 2)) ** d + regulator))
+        return (integrand, 0.)
 
 
 random.seed(123)