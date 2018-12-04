ce6ebd958c19453b5e71e00875e54c8bac60dd24
Valentin Hirschi <valentin.hirschi@gmail.com>
2018-12-04 14:07:24 +0100

    Improved probe_hyperparameters.py so as to save data on disk on-the-fly and improved statistics printed out.

--- rust_backend/probe_hyperparameters.py
+++ rust_backend/probe_hyperparameters.py
@@ -2,8 +2,9 @@
 
 import sys
 import os
-import shutil
+import itertools
 import subprocess
+import time
 import yaml
 from yaml import Loader, Dumper
 from pprint import pprint
@@ -17,15 +18,8 @@ class HyperParamProber(object):
                  rust_config_file_path = pjoin(os.getcwd(),'config.yaml'),
                  rust_executable_path = pjoin(os.getcwd(),'target','release','integrator'),
                  topology = 'box',
-                 additional_run_options = {
-                     'min_eval'   : 0,
-                     'nstart'     : 1000000,
-                     'nincrease'  : 5000,
-                     'max_eval'   : 50000000,
-                     'cores'      : 0,
-                     'seed'       : 1,
-                     'refine_n_runs' : 0
-                 }
+                 log_stream = None,
+                 additional_run_options = None
                 ):
         self._rust_config_file_path = rust_config_file_path
         self._rust_executable = rust_executable_path
@@ -39,10 +33,27 @@ class HyperParamProber(object):
         # Apply additional run options 
         # Save the VEGAS state to a dummy file that will need to be removed after each run
         self._default_run_configuration['state_filename'] = self._DUMMY_VEGAS_STATE_FILE_PATH
+        self.log_stream = log_stream
+        # Assign some default additional run options if not specified:
+        if additional_run_options is None:
+            additional_run_options = {
+                     'min_eval'   : 0,
+                     'nstart'     : 1000000,
+                     'nincrease'  : 5000,
+                     'max_eval'   : 50000000,
+                     'cores'      : 0,
+                     'seed'       : 1,
+                     'refine_n_runs' : 0
+                 }
         for option, value in additional_run_options.items():
             if option not in self._default_run_configuration:
                 raise BaseException("Could not find parameter '%s' in the the config yaml file."%option)
             self._default_run_configuration[option] = value
+
+        # Now save the default yaml configuration to the log stream if specified.
+        if log_stream is not None:
+            log_stream.write(yaml.dump([('default_configuration',self._default_run_configuration),], Dumper=Dumper))
+
 
     def run_rust(self):
         """ Steer a run of rust integration with the current yaml config setup and return the result."""
@@ -111,6 +122,9 @@ class HyperParamProber(object):
                 except Exception as e:
                     print('An error occurred when running rust: %s'%str(e))
                     raise e
+                # Save the result into the logstream if specified
+                if log_stream is not None:
+                    log_stream.write(yaml.dump([('run_result', (param_combination,run_result) ), ], Dumper=Dumper))
                 print("Result of the run:")
                 pprint(run_result)
                 all_results.append((param_combination,run_result))
@@ -123,26 +137,42 @@ class HyperParamProber(object):
             except:
                 pass
             try:
-                os.remove(_DUMMY_VEGAS_STATE_FILE_PATH)
+                os.remove(self._DUMMY_VEGAS_STATE_FILE_PATH)
             except:
                 pass
 
             print("Run interrupted by user, partial results will be returned")
 
-        return all_results
+        return {
+            'default_configuration' : self._default_run_configuration,
+            'run_results'           : all_results
+        }
 
 class ResultsAnalyser(object):
     
     def __init__(self, results):
-        self.results = results
+        self.default_configuration = results['default_configuration']
+        self.results = results['run_results']
+        self.run_time = results['run_time']
 
     def analyse(self, n_optimal_to_show=5):
 
         # List all parameters studied
         all_params = sorted(list(set(sum((r[0].keys() for r in self.results),[]))))
-        print('='*40)
+        print('='*80)
+        print("Statistics considered for topology '%s':"%self.default_configuration['active_toplogy'])
+        stats_params = ['nstart','nincrease','max_eval']
+        for param in stats_params:
+            print('  %-20s = %d' % (param, self.default_configuration[param]))
+        print('-'*80)
+        print("run time: %.3e [h]"%(self.run_time/3600.0))
+        print('-'*80)
+        print("Default parameter values:")
+        for param in all_params:
+            print('  %-20s = %.3e'%(param, self.default_configuration[param]))
+        print('-'*80)
         print("Parameters studied: %s"%(', '.join(all_params)))
-        print('-'*40)
+        print('-'*80)
         default_result = None
         for r in self.results:
             if r[0]=={}:
@@ -151,28 +181,42 @@ class ResultsAnalyser(object):
         if default_result is None:
             print('WARNING: Could not find default result.')
         else:
-            print('Default MC error: %.3e'%default_result[1]['error'][0])
+            print('%-50s%.3e +- %.3e'%('Default -> ',default_result[1]['result'][0],default_result[1]['error'][0]))
         print('-'*40)            
         for param in all_params:
             sorted_param_values = sorted([r for r in self.results if param in r[0]], key=lambda el: el[1]['error'][0]) 
             print("First %d optimal values for parameter '%s':"%(n_optimal_to_show, param))
             for i_optimal, result in enumerate(sorted_param_values[:n_optimal_to_show]):
-                print("  | optimal values #%d: %s -> MC_error: %.3e"%(
-                    i_optimal, 
+                print("%-30s%-20s -> %.3e +- %.3e"%(
+                    '  | optimal values #%d: '%i_optimal,
                     ', '.join('%s=%g'%(p,result[0][p]) for p in sorted(result[0].keys()) ),
+                    result[1]['result'][0],
                     result[1]['error'][0]
                 ))
-        print('-'*40)
+        print('-'*80)
         print("Overall %d best results:"%n_optimal_to_show)
         sorted_all_param_values = sorted(self.results, key=lambda el: el[1]['error'][0]) 
         for i_optimal, result in enumerate(sorted_all_param_values[:n_optimal_to_show]):
-            print("  | optimal values #%d: %s -> MC_error: %.3e"%(
-                i_optimal, 
+            print("%-30s%-20s -> %.3e +- %.3e"%(
+                '  | optimal values #%d: '%i_optimal,
                 ', '.join('%s=%g'%(p,result[0][p]) for p in sorted(result[0].keys()) ),
+                result[1]['result'][0],
                 result[1]['error'][0]
             ))
-        print('='*40)
+        print('='*80)
 
+
+def load_results_from_yaml(log_file_path):
+    """Load a full-fledged scan from a yaml dump"""
+
+    raw_data = yaml.load(open(log_file_path,'r'), Loader=Loader)
+    processed_data = {'run_results': []}
+    for entry_name, entry_value in raw_data:
+        if entry_name == 'run_result':
+            processed_data['run_results'].append(entry_value)
+        else:
+            processed_data[entry_name] = entry_value
+    return processed_data
 
 if __name__ == '__main__':
     prog_options = ' '.join(sys.argv[1:]).split('--')
@@ -197,7 +241,7 @@ if __name__ == '__main__':
     topology = 'box'
     n_optimal_to_show = 5
     load_results_from = None
-    save_results_to = pjoin(os.getcwd(),'hyperparameters_scan.dat')
+    save_results_to = pjoin(os.getcwd(),'hyperparameters_scan.yaml')
     for prog_option in prog_options:
         if prog_option.strip()=='':
             continue
@@ -221,19 +265,23 @@ if __name__ == '__main__':
         else:
             # Unknown options will be considered by default as additional value to give to rust
             additional_run_options[option_name] = eval(option_value)
-    
-    prober = HyperParamProber(
-        rust_config_file_path = rust_config_file_path,
-        topology = topology,
-        additional_run_options = additional_run_options
-    )
+
     if load_results_from is None:
+        log_stream = open(save_results_to,'w')
+        prober = HyperParamProber(
+            rust_config_file_path=rust_config_file_path,
+            topology=topology,
+            additional_run_options=additional_run_options,
+            log_stream=log_stream
+        )
+        start_time = time.time()
         test_results = prober.probe(parameter_values_to_test, correlated_variations=False)
-        # Dump results to a file for possible later use
-        open(save_results_to,'w').write(yaml.dump(test_results, Dumper=Dumper))
+        test_results['run_time'] = time.time()-start_time
+        log_stream.write(yaml.dump([('run_time', test_results['run_time']), ], Dumper=Dumper))
+        log_stream.close()
     else:
         try:
-            test_results = yaml.load(open(load_results_from,'r'), Loader=Loader)
+            test_results = load_results_from_yaml(load_results_from)
         except Exception as e:
             print("ERROR: Could not hyperparameters scan results from file '%s'. Error: %s"%(
                 load_results_from, str(e)
