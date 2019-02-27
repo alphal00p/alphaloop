905c0ca57235de99331dd95b7871582daa59625a
Valentin Hirschi <valentin.hirschi@gmail.com>
2019-02-27 19:32:16 +0100

    Added analytic results to topologies. Added DoubleBox and TriangleBox topologies all following Weinzierls conventions.

--- LTD/ltd_commons.py
+++ LTD/ltd_commons.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import vectors
+import math
 zero_lv = vectors.LorentzVector([0.,0.,0.,0.])
 
 #############################################################################################################
@@ -10,7 +11,7 @@ zero_lv = vectors.LorentzVector([0.,0.,0.,0.])
 class LoopTopology(object):
     """ A simple container for describing a loop topology."""
 
-    def __init__(self, ltd_cut_structure, loop_lines, external_kinematics, n_loops=1, name=None, **opts):
+    def __init__(self, ltd_cut_structure, loop_lines, external_kinematics, n_loops=1, name=None, analytical_result=None, **opts):
         """
             loop_lines          : A tuple of loop lines instances corresponding to each edge of the directed
                                   graph of this topology.
@@ -26,6 +27,10 @@ class LoopTopology(object):
         self.ltd_cut_structure   = ltd_cut_structure
         self.n_loops             = n_loops 
         self.name                = name
+        if callable(analytical_result):
+            self.analytical_result   = analytical_result(self.external_kinematics)
+        else:
+            self.analytical_result   = analytical_result
         
     def print_topology(self):
         """ Print the topology using graphviz."""
@@ -95,6 +100,7 @@ class LoopTopology(object):
         res['n_loops'] = self.n_loops
         res['loop_lines'] = [ll.to_flat_format() for ll in self.loop_lines]
         res['external_kinematics'] = [ [float(v) for v in vec] for vec in self.external_kinematics]
+        res['analytical_result'] = float(self.analytical_result) if self.analytical_result else 0.
 
         return res
 
@@ -107,7 +113,8 @@ class LoopTopology(object):
             ltd_cut_structure   =   tuple([tuple(cs) for cs in flat_dict['ltd_cut_structure']]),
             n_loops             =   flat_dict['n_loops'],
             loop_lines          =   tuple([LoopLine.from_flat_format(ll) for ll in flat_dict['loop_lines']]),
-            external_kinematics =   vectors.LorentzVectorList([vectors.LorentzVector(v) for v in flat_dict['external_kinematics']])
+            external_kinematics =   vectors.LorentzVectorList([vectors.LorentzVector(v) for v in flat_dict['external_kinematics']]),
+            analytical_result   =   (None if flat_dict['analytical_result']==0. else flat_dict['analytical_result'])
         ) 
 
     @staticmethod
@@ -281,22 +288,28 @@ class TopologyCollection(dict):
 # Definition of hard-coded topologies.
 #############################################################################################################
 
-def create_hard_coded_topoloogy(name,external_momenta):
+def create_hard_coded_topoloogy(name, external_momenta, analytical_result=None):
     """ Creates a hard-coded topology of the given name with specified kinematics. 
     
     ================================================
-    /!\ ALL EXTERNAL MOMENTA ARE DEFINED AS OUTGOING
+    /!\ ALL EXTERNAL MOMENTA ARE DEFINED AS INCOMING 
     ================================================
-    
+   
+    Convention for loop normalisation in analytical result is 
+        (1./ 2.*math.pi)**(4*n_loops)
+
     """
 
     if name == 'DoubleTriangle':
         p1 = external_momenta[0]
         p2 = external_momenta[1]
         return LoopTopology(
-            name    = 'DoubleTriange',
+            name    = 'DoubleTriangle',
             n_loops = 2,
             external_kinematics = external_momenta,
+            # Analytical result is simple and should be -(6*Zeta[3])/((16*pi^2)^2 s)
+            # so we can use the analytical result here.
+            analytical_result = (lambda ps: -0.00028922566024/ps[0].square()),
             ltd_cut_structure = (
                 (LoopLine.NEGATIVE_CUT  , LoopLine.NO_CUT           , LoopLine.POSITIVE_CUT ),
                 (LoopLine.POSITIVE_CUT  , LoopLine.POSITIVE_CUT     , LoopLine.NO_CUT       ),
@@ -308,7 +321,7 @@ def create_hard_coded_topoloogy(name,external_momenta):
                     end_node    = 2,
                     signature   = (1,0),
                     propagators = (
-                        Propagator(q=p1, m_squared=0.),
+                        Propagator(q=-p1, m_squared=0.),
                         Propagator(q=zero_lv, m_squared=0.),
                     )
                 ),
@@ -317,7 +330,7 @@ def create_hard_coded_topoloogy(name,external_momenta):
                     end_node    = 2,
                     signature   = (0,1),
                     propagators = (
-                        Propagator(q=p2, m_squared=0.),
+                        Propagator(q=-p2, m_squared=0.),
                         Propagator(q=zero_lv, m_squared=0.),
                     )
                 ),
@@ -332,6 +345,97 @@ def create_hard_coded_topoloogy(name,external_momenta):
             ) 
         )
 
+    elif name == 'TriangleBox':
+        p1 = external_momenta[0]
+        p2 = external_momenta[1]
+        p3 = external_momenta[2]
+        return LoopTopology(
+            name    = 'TriangleBox',
+            n_loops = 2,
+            external_kinematics = external_momenta,
+            analytical_result = analytical_result,
+            ltd_cut_structure = (
+                (LoopLine.NEGATIVE_CUT  , LoopLine.NO_CUT           , LoopLine.POSITIVE_CUT ),
+                (LoopLine.POSITIVE_CUT  , LoopLine.POSITIVE_CUT     , LoopLine.NO_CUT       ),
+                (LoopLine.NO_CUT        , LoopLine.POSITIVE_CUT     , LoopLine.POSITIVE_CUT ),
+            ),
+            loop_lines = (
+                LoopLine(
+                    start_node  = 1, 
+                    end_node    = 2,
+                    signature   = (1,0),
+                    propagators = (
+                        Propagator(q=-p3, m_squared=0.),
+                        Propagator(q=zero_lv, m_squared=0.),
+                    )
+                ),
+                LoopLine(
+                    start_node  = 1, 
+                    end_node    = 2,
+                    signature   = (0,1),
+                    propagators = (
+                        Propagator(q=-p1-p2, m_squared=0.),
+                        Propagator(q=-p1, m_squared=0.),                        
+                        Propagator(q=zero_lv, m_squared=0.),
+                    )
+                ),
+                LoopLine(
+                    start_node  = 2, 
+                    end_node    = 1,
+                    signature   = (1,1),
+                    propagators = (
+                        Propagator(q=zero_lv, m_squared=0.),
+                    )
+                ),
+            ) 
+        )
+
+    elif name == 'DoubleBox':
+        p1 = external_momenta[0]
+        p2 = external_momenta[1]
+        p3 = external_momenta[2]
+        p4 = external_momenta[3]
+        return LoopTopology(
+            name    = 'DoubleBox',
+            n_loops = 2,
+            external_kinematics = external_momenta, 
+            analytical_result = analytical_result,
+            ltd_cut_structure = (
+                (LoopLine.NEGATIVE_CUT  , LoopLine.NO_CUT           , LoopLine.POSITIVE_CUT ),
+                (LoopLine.POSITIVE_CUT  , LoopLine.POSITIVE_CUT     , LoopLine.NO_CUT       ),
+                (LoopLine.NO_CUT        , LoopLine.POSITIVE_CUT     , LoopLine.POSITIVE_CUT ),
+            ),
+            loop_lines = (
+                LoopLine(
+                    start_node  = 1, 
+                    end_node    = 2,
+                    signature   = (1,0),
+                    propagators = (
+                        Propagator(q=-p1-p2, m_squared=0.),
+                        Propagator(q=-p2, m_squared=0.),                        
+                        Propagator(q=zero_lv, m_squared=0.),
+                    )
+                ),
+                LoopLine(
+                    start_node  = 1, 
+                    end_node    = 2,
+                    signature   = (0,1),
+                    propagators = (
+                        Propagator(q=-p4-p3, m_squared=0.),
+                        Propagator(q=-p3, m_squared=0.),                        
+                        Propagator(q=zero_lv, m_squared=0.),
+                    )
+                ),
+                LoopLine(
+                    start_node  = 2, 
+                    end_node    = 1,
+                    signature   = (1,1),
+                    propagators = (
+                        Propagator(q=zero_lv, m_squared=0.),
+                    )
+                ),
+            ) 
+        )
 
     else:
         raise BaseException("Unknown hard-coded topology name '%s'"%name)
@@ -343,25 +447,56 @@ def create_hard_coded_topoloogy(name,external_momenta):
 hard_coded_topology_collection = TopologyCollection()
 
 # Add the double-triangle to the hard-coded topology collection:
+hard_coded_topology_collection.add_topology(   
+    create_hard_coded_topoloogy(
+        'DoubleTriangle',
+        vectors.LorentzVectorList([
+                vectors.LorentzVector([1.,0.3,0.4,0.5]),
+                vectors.LorentzVector([-1.,-0.3,-0.4,-0.5])
+        ])
+        # Analytical is given by its exact function directly for 'DoubleTriangle'        
+    ),
+)
+
 hard_coded_topology_collection.add_topology(
     create_hard_coded_topoloogy(
-
-        'DoubleTriangle', 
+        'TriangleBox',
         vectors.LorentzVectorList([
-                vectors.LorentzVector([-1.,-0.3,-0.4,-0.5]),
-                vectors.LorentzVector([1.,0.3,0.4,0.5])
-        ])
+                vectors.LorentzVector([39.7424,-14.1093,0.102709,20.4908]),
+                vectors.LorentzVector([50.2576,14.1093,-0.102709,-20.4908]),
+                vectors.LorentzVector([-90.,0.,0.,0.]),
+        ]),
+        # Analytical result known but exact numerical result simply copied here from C^(2) of table 1
+        # https://arxiv.org/pdf/1211.0509.pdf
+        analytical_result = -1.832e-11
+    ),
+)
 
-    )
+hard_coded_topology_collection.add_topology(
+    create_hard_coded_topoloogy(
+        'DoubleBox',        
+        vectors.LorentzVectorList([
+                vectors.LorentzVector([19.6586,-7.15252,-0.206016,8.96383]),
+                vectors.LorentzVector([26.874,7.04203,-0.0501295,-12.9055]),
+                vectors.LorentzVector([43.4674,0.110491,0.256146,3.9417]),
+                vectors.LorentzVector([-90.,0.,0.,0.]),
+        ]),
+        # Analytical result known but exact numerical result simply copied here from D^(2) of table 1 of
+        # https://arxiv.org/pdf/1211.0509.pdf
+        analytical_result =  -5.897e-14
+    ),
 )
 
 # Example printout
 # ----------------
 #hard_coded_topology_collection['DoubleTriange'].print_topology()
+#hard_coded_topology_collection['DoubleTriangle'].print_topology()
+#hard_coded_topology_collection['TriangleBox'].print_topology()
+#hard_coded_topology_collection['DoubleBox'].print_topology()
 
 # Example of yaml export and import
 # ---------------------------------
-#hard_coded_topology_collection['DoubleTriange'].export_to('ExampleDoubleTriangleExport.yaml')
+#hard_coded_topology_collection['DoubleTriangle'].export_to('ExampleDoubleTriangleExport.yaml')
 #test = LoopTopology.import_from('ExampleDoubleTriangleExport.yaml')
 #test.print_topology()
 
