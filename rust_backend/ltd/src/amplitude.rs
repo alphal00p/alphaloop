c77f5042b48cccab63a79c8dc03bf079f1c33a03
Andrea Pelloni <apelloni90@gmail.com>
2019-08-19 10:36:27 +0200

    eeAA amplitude and polarizations

--- rust_backend/ltd/src/amplitude.rs
+++ rust_backend/ltd/src/amplitude.rs
@@ -4,6 +4,7 @@ use utils;
 use vector::LorentzVector;
 use FloatLike;
 
+#[allow(non_camel_case_types, dead_code)]
 enum Parameters {
     g_f,
     alpha_s,
@@ -20,6 +21,7 @@ fn parameters(parameter: Parameters) -> f64 {
     }
 }
 
+#[allow(non_camel_case_types, dead_code)]
 enum Polarizations {
     UPlus,
     UMinus,
@@ -29,7 +31,8 @@ enum Polarizations {
     AMinus,
 }
 
-pub fn compute_polarization(
+#[allow(unused_variables)]
+fn compute_polarization(
     p: LorentzVector<f64>,
     polarization: Polarizations,
 ) -> Result<Vec<Complex<f64>>, &'static str> {
@@ -83,9 +86,9 @@ pub fn compute_polarization(
                     return Ok(u_plus);
                 }
                 _ => {
-                    let u_bar_plus = Vec::new();
+                    let mut u_bar_plus = Vec::new();
                     for g in gamma0.iter() {
-                        let res = Complex::default();
+                        let mut res = Complex::default();
                         for (i, x) in u_plus.iter().enumerate() {
                             res += x * g[i];
                         }
@@ -116,9 +119,9 @@ pub fn compute_polarization(
                     return Ok(u_minus);
                 }
                 _ => {
-                    let u_bar_minus = Vec::new();
+                    let mut u_bar_minus = Vec::new();
                     for g in gamma0.iter() {
-                        let res = Complex::default();
+                        let mut res = Complex::default();
                         for (i, x) in u_minus.iter().enumerate() {
                             res += x * g[i];
                         }
@@ -141,7 +144,7 @@ pub fn compute_polarization(
             //helicity eigenstates of photons
             let ii = Complex::new(0.0, 1.0);
             let norm = 1. / (2.0 as f64).sqrt();
-            let a_plus = Vec::new();
+            let mut a_plus = Vec::new();
             for i in 0..a1.len() {
                 a_plus.push(norm * (-a1[i] - ii * a2[i]));
             }
@@ -160,7 +163,7 @@ pub fn compute_polarization(
             //helicity eigenstates of photons
             let ii = Complex::new(0.0, 1.0);
             let norm = 1. / (2.0 as f64).sqrt();
-            let a_minus = Vec::new();
+            let mut a_minus = Vec::new();
             for i in 0..a1.len() {
                 a_minus.push(norm * (a1[i] - ii * a2[i]));
             }
@@ -169,17 +172,21 @@ pub fn compute_polarization(
         }
     }
 }
+//END compute_polarization
+#[derive(Debug)]
+#[allow(unused_variables, non_camel_case_types)]
 pub struct eeAA<T: FloatLike> {
     external_kinematics: Vec<LorentzVector<f64>>,
     den: Vec<Complex<T>>,
     vectors: Vec<LorentzVector<Complex<T>>>,
     vbar: Vec<Complex<T>>,
     u: Vec<Complex<T>>,
-    a_mu: Vec<Complex<T>>,
-    a_nu: Vec<Complex<T>>,
+    a_mu: LorentzVector<Complex<T>>,
+    a_nu: LorentzVector<Complex<T>>,
     mu_uv: Complex<T>,
 }
 
+#[allow(unused_variables, non_camel_case_types)]
 impl<T: FloatLike> eeAA<T> {
     pub fn new(
         external_kinematics: Vec<LorentzVector<f64>>,
@@ -201,18 +208,25 @@ impl<T: FloatLike> eeAA<T> {
             .collect();
 
         //random contraction for the offshell photons
-        let a_mu = LorentzVector::new(1., 2., 3., 4.)
-            .iter()
-            .map(|x| Complex::new(T::from_f64(x.re).unwrap(), T::from_f64(x.im).unwrap()))
-            .collect();
+        let a_mu = LorentzVector::from_args(
+            Complex::new(T::from_f64(1.).unwrap(), T::zero()),
+            Complex::new(T::from_f64(2.).unwrap(), T::zero()),
+            Complex::new(T::from_f64(3.).unwrap(), T::zero()),
+            Complex::new(T::from_f64(4.).unwrap(), T::zero()),
+        );
 
-        let a_nu = LorentzVector::new(2., 3., 5., 7.)
-            .iter()
-            .map(|x| Complex::new(T::from_f64(x.re).unwrap(), T::from_f64(x.im).unwrap()))
-            .collect();
+        let a_nu = LorentzVector::from_args(
+            Complex::new(T::from_f64(2.).unwrap(), T::zero()),
+            Complex::new(T::from_f64(3.).unwrap(), T::zero()),
+            Complex::new(T::from_f64(5.).unwrap(), T::zero()),
+            Complex::new(T::from_f64(7.).unwrap(), T::zero()),
+        );
 
         let k = loop_momentum;
-        let ps = external_kinematics;
+        let ps: Vec<LorentzVector<Complex<T>>> = external_kinematics
+            .iter()
+            .map(|mom| (*mom).map(|x| Complex::new(T::from_f64(x).unwrap(), T::zero())))
+            .collect();
         let vectors = vec![
             k + ps[0],
             -k - ps[0] - ps[1],
@@ -234,96 +248,83 @@ impl<T: FloatLike> eeAA<T> {
         });
     }
 
+    pub fn compute_chain(&mut self, indices: Vec<i8>) -> Result<Complex<T>, &'static str> {
+        GammaChain::new(
+            self.vbar.clone(),
+            self.u.clone(),
+            indices,
+            self.vectors.clone(),
+        )
+        .unwrap()
+        .compute_chain()
+    }
+
     pub fn compute_amplitude(&mut self) -> Result<Complex<T>, &'static str> {
-        let numerator = Complex::default() - 1;
+        let mut numerator: Complex<T> = Complex::new(-T::one(), T::zero());
 
         //invariants
-        let s12 = (self.external_kinematics[0] + self.external_kinematics[1]).square();
-        let s23 = (self.external_kinematics[1] + self.external_kinematics[2]).square();
-        let factor = (parameters(Parameters::alpha_ew) * 4.0 * std::f64::consts::PI).pow(2);
+        let s12 = T::from_f64((self.external_kinematics[0] + self.external_kinematics[1]).square())
+            .unwrap();
+        let s23 = T::from_f64((self.external_kinematics[1] + self.external_kinematics[2]).square())
+            .unwrap();
+        let factor =
+            T::from_f64((parameters(Parameters::alpha_ew) * 4.0 * std::f64::consts::PI).powf(2.0))
+                .unwrap();
 
         /* ====== DIAGRAMS ====== */
         //D1
+        let den1 = self.den[1];
+
         let indices = vec![6, 5, -1, 3, 7, 4, -1];
-        let chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap();
-        numerator += -factor / s23 * chain_res * self.den[1];
+        let chain_res = self.compute_chain(indices).unwrap();
+        numerator += chain_res * (-factor / s23) * den1;
 
         //D2
         let indices = vec![-1, 2, 6, 3, -1, 5, 7];
-        let chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap();
-        numerator += -factor / s23 * chain_res * self.den[3];
+        let chain_res = self.compute_chain(indices).unwrap();
+        numerator += chain_res * (-factor / s23) * &self.den[3];
 
         //D3
         let indices = vec![-1, 2, 6, 3, 7, 4, -1];
-        let chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap();
-        numerator += -factor * chain_res;
+        let chain_res = self.compute_chain(indices).unwrap();
+        numerator += chain_res * (-factor);
 
         //D4
         let indices = vec![6, 5, -1, 3, -1, 5, 7];
-        let chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap();
-        numerator += -factor / s23.pow(2) * chain_res * (self.den[2] + self.den[4]);
+        let chain_res = self.compute_chain(indices).unwrap();
+        numerator += chain_res * (-factor / s23 / s23) * (self.den[2] + self.den[4]);
 
         /* ====== COUNTERTERMS ====== */
         let den_uv = self.den[0] - self.mu_uv;
-        let box_den = self.den[0] * self.den[1] * self.den[2] * self.den[3];
+        let den_uv_sq = den_uv * den_uv;
+
+        let box_den = self.den[0] * den1 * self.den[2] * self.den[3];
         //IR
         let indices = vec![-1, 2, 6, 5, 7, 4, -1];
-        let chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap();
-        numerator -= -factor / s23.pow(2) * chain_res * self.den[2];
+        let chain_res = self.compute_chain(indices).unwrap();
+        numerator -= chain_res * (-factor / s23 / s23) * self.den[2];
 
         //UV1
         let indices = vec![6, 5, -1, 1, 7, 1, -1];
-        let chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap();
-        numerator -= -factor / s23.pow(2) * chain_res * box_den * utils::finv(den_uv.pow(3));
+        let chain_res = self.compute_chain(indices).unwrap();
+        numerator -= chain_res * (-factor / s23 / s23) * box_den * utils::finv(den_uv * den_uv_sq);
 
         //UV2
         let indices = vec![-1, 1, 6, 1, -1, 5, 7];
-        let chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap();
-        numerator -= -factor / s23.pow(2) * chain_res * box_den * utils::finv(den_uv.pow(3));
+        let chain_res = self.compute_chain(indices).unwrap();
+        numerator -= chain_res * (-factor / s23 / s23) * box_den * utils::finv(den_uv * den_uv_sq);
 
         //UV4
         let indices = vec![6, 5, -1, 1, -1, 5, 7];
-        let mut chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap()
-            * utils::finv(den_uv.pow(2));
+        let mut chain_res = self.compute_chain(indices).unwrap() * utils::finv(den_uv_sq);
         let indices = vec![6, 5, -1, 1, 5, 1, -1, 5, 7];
-        chain_res -= GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap()
-            * utils::finv(den_uv.pow(3));
-        numerator -= -factor / s23.pow(2) * chain_res * box_den;
+        chain_res -= self.compute_chain(indices).unwrap() * utils::finv(den_uv * den_uv_sq);
+        numerator -= chain_res * (-factor / s23 / s23) * box_den;
 
         //UVIR
         let indices = vec![-1, 1, 6, 5, 7, 1, -1];
-        let chain_res = GammaChain::new(self.vbar, self.v, indices, self.vectors)
-            .unwrap()
-            .compute_chain()
-            .unwrap();
-        numerator -= factor / s23.pow(2) * chain_res * box_den * utils::finv(den_uv.pow(3));
+        let chain_res = self.compute_chain(indices).unwrap();
+        numerator -= chain_res * (factor / s23 / s23) * box_den * utils::finv(den_uv * den_uv_sq);
 
         return Ok(numerator);
     }
