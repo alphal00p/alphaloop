23e9369d522e061d8b0407d8dc52d1d45bbb3ffd
ValentinHirschi <valentin.hirschi@gmail.com>
2019-10-21 20:11:59 +0200

    Fixed AVHOneLOopHook to force python3

--- LTD/AVHOneLOopHook/test.py
+++ LTD/AVHOneLOopHook/test.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python3 
 import math
 import pyAVH_OneLOop_hook
 result = pyAVH_OneLOop_hook.compute_one_loop_box(100.**2,200.**2,300.**2,400.**2,812438.8277451771e0,-406042.8596833937e0,0.,0.,0.,0.)
-print "BOX: Single pole obtained: %.16e %s %.16e*i"%(result[1].real, '+' if result[1].real > 0. else '-', abs(result[1].imag))
-print "BOX: Double pole obtained: %.16e %s %.16e*i"%(result[2].real, '+' if result[2].real > 0. else '-', abs(result[2].imag))
-print "BOX: The poles should be zero."
-print "BOX: The finite part of the result obained is: %.16e %s %.16e*i"%(result[0].real, '+' if result[0].real > 0. else '-', abs(result[0].imag))
-print "BOX: The finite part of the result should be : %.16e - %.16e*i"%( -5.0195667135415558e-11, 9.2157738446831588e-11) 
+print("BOX: Single pole obtained: %.16e %s %.16e*i"%(result[1].real, '+' if result[1].real > 0. else '-', abs(result[1].imag)))
+print("BOX: Double pole obtained: %.16e %s %.16e*i"%(result[2].real, '+' if result[2].real > 0. else '-', abs(result[2].imag)))
+print("BOX: The poles should be zero.")
+print("BOX: The finite part of the result obained is: %.16e %s %.16e*i"%(result[0].real, '+' if result[0].real > 0. else '-', abs(result[0].imag)))
+print("BOX: The finite part of the result should be : %.16e - %.16e*i"%( -5.0195667135415558e-11, 9.2157738446831588e-11)) 
 result = pyAVH_OneLOop_hook.compute_one_loop_triangle(1,1./4.,1./16.,0.,0.,0.)
 print(result)
 #result = pyAVH_OneLOop_hook.compute_one_loop_triangle(1.,1.,1.,0.,0.,0.)
 # Below is an exceptional point that leads to unstable evaluations...
 #result = pyAVH_OneLOop_hook.compute_one_loop_triangle(1.,4.,9.,0.,0.,0.)
-print "TRIANGLE: Single pole obtained: %.16e %s %.16e*i"%(result[1].real, '+' if result[1].real > 0. else '-', abs(result[1].imag))
-print "TRIANGLE: Double pole obtained: %.16e %s %.16e*i"%(result[2].real, '+' if result[2].real > 0. else '-', abs(result[2].imag))
-print "TRIANGLE: The poles should be zero."
-print "TRIANGLE: The finite part of the result obained is: %.16e %s %.16e*i"%(result[0].real, '+' if result[0].real > 0. else '-', abs(result[0].imag))
-print "TRIANGLE: The finite part of the result should be : %.16e + %.16e*i"%( 7.8701840491118205e+00 , 0.0000000000000000e+00 )
+print("TRIANGLE: Single pole obtained: %.16e %s %.16e*i"%(result[1].real, '+' if result[1].real > 0. else '-', abs(result[1].imag)))
+print("TRIANGLE: Double pole obtained: %.16e %s %.16e*i"%(result[2].real, '+' if result[2].real > 0. else '-', abs(result[2].imag)))
+print("TRIANGLE: The poles should be zero.")
+print("TRIANGLE: The finite part of the result obained is: %.16e %s %.16e*i"%(result[0].real, '+' if result[0].real > 0. else '-', abs(result[0].imag)))
+print("TRIANGLE: The finite part of the result should be : %.16e + %.16e*i"%( 7.8701840491118205e+00 , 0.0000000000000000e+00 ))
 
-print(pyAVH_OneLOop_hook.compute_one_loop_triangle(-1.,1.,1.,0.,0.,0.))
+print((pyAVH_OneLOop_hook.compute_one_loop_triangle(-1.,1.,1.,0.,0.,0.)))
 
 print("----------------")
 #result = pyAVH_OneLOop_hook.compute_one_loop_box(-100.,0.**2,0.**2,0.**2, -100.,-100.,0.,0.,0.,0.)
@@ -31,7 +31,7 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     10240000.000000000,
     -2826958.6951293740,
     800.**2,200.**2,400.**2,600.**2)
-print result
+print(result)
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
     100.0**2,
     300.0**2,
@@ -40,7 +40,7 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     10240000.000000000,
     -5950922.6967353411,
     800.**2,200.**2,400.**2,600.**2)
-print result
+print(result)
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -10000.000000000000,
     -90000.000000000000,
@@ -49,7 +49,7 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -10240000.000000000,
     1880272.8264859747,
     800.**2,200.**2,400.**2,600.**2)
-print result 
+print(result) 
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -4869.270805985198,
@@ -60,8 +60,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     4533.972680586998,
      9.82998**2,9.82998**2,9.82998**2,9.82998**2)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
     0.7,
@@ -72,8 +72,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -0.93,
      0.,0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 print('==========')
 
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
@@ -85,8 +85,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -0.68,
      0.,0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
     0.5875,
@@ -97,8 +97,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -0.9688,
      0.,0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
     0.84,
@@ -109,8 +109,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -3.45,
      0.,0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
@@ -122,8 +122,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -0.93,
      0.,0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
@@ -132,8 +132,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
     -0.77,
      0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
     0.7,
@@ -141,8 +141,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
     -0.32,
      0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
     0.72308431,
@@ -150,8 +150,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
     -0.55763794,
      0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 
 print('==========')
@@ -164,8 +164,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -0.99810302,
      0., 0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 
 print('==========')
@@ -178,8 +178,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -0.93,
      0., 0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 
 print('==========')
@@ -192,8 +192,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -3.45,
      0., 0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_box(
@@ -205,8 +205,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -0.9688,
      0., 0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 
 print('==========')
@@ -219,8 +219,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_box(
     -1.1897703974737204,
      0., 0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 
 print('==========')
@@ -230,8 +230,8 @@ result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
      -367.0,
     0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
 
 print('==========')
 result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
@@ -240,5 +240,5 @@ result = pyAVH_OneLOop_hook.compute_one_loop_triangle(
      70.0,
     0.,0.,0.)
 result[0]*=(1.j/(16.*(math.pi**2)))
-print result
-print '%.16e + %.16e'%(result[0].real, result[0].imag)
+print(result)
+print('%.16e + %.16e'%(result[0].real, result[0].imag))
