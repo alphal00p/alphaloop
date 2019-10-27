82ce88ba9cc1f39a0f809379596ba77d01d2df07
ValentinHirschi <valentin.hirschi@gmail.com>
2019-10-27 11:29:36 +0100

    Added a new module Deformation_paper_topologies.py for specifying the topologies we want to run for the paper. This time a load function allows to sepcify which ones to export, if not all.

---
+++ LTD/Deformation_paper_topologies.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import os
+import vectors
+import math
+
+import mpmath
+import numpy
+from scipy.special import zeta
+
+zero_lv = vectors.LorentzVector([0.,0.,0.,0.])
+
+from ltd_utils import LoopTopology, LoopLine, Propagator, TopologyCollection, TopologyGenerator
+from analytical_expressions  import ladder_phi, analytic_four_point_ladder, analytic_three_point_ladder, analytic_two_point_ladder
+
+#############################################################################################################
+# Create the collection of hard-coded topologies.
+#############################################################################################################
+
+# Add Now automatically generated topologies
+def load(selected_topologies=None):
+
+    all_topologies = TopologyCollection()
+
+    topology_name = "T1_Pentabox_physical"
+    if selected_topologies is None or topology_name in selected_topologies:
+
+        # This topology correspond to a two-loop pentabox with physical kinematics borrowed from
+        # the process `t t~ > w+ z w-` at 1 TeV E_com.
+        q1 = vectors.LorentzVector(
+            # mass: 173.0 incoming top quark
+            [0.5980260048915123e+03,   0.0000000000000000e+00,   0.0000000000000000e+00,   0.5724562014045295e+03]
+        ) 
+        q2 = vectors.LorentzVector(
+            # mass: 173.0 incoming anti-top quark
+            [0.5980260048915123e+03,   0.0000000000000000e+00,   0.0000000000000000e+00,   -0.5724562014045295e+03]
+        )
+        q3 = vectors.LorentzVector(
+            # mass: 80.419 outgoing W+
+            [-0.5394473213122507e+03,   -0.1971081698462961e+03,   -0.4416135519343869e+03,  0.2250822886064787e+03]
+        )
+        q4 = vectors.LorentzVector(
+            # mass: 91.188 outgoing Z
+            [-0.2255538754188549e+03,  0.1757868459829899e+03,   0.3716353112335996e+02,   -0.1013763093935658e+03]            
+        )
+        # mass: 80.419 outgoing W-
+        q5 = q4-q3-q2-q1
+
+        factory = TopologyGenerator([
+            ('p1', 1, 6), ('p2', 6, 7), ('p3', 7, 2), ('p4', 2, 1),  
+            ('p5', 7, 3), ('p6', 3, 4), ('p7', 4, 5), ('p8', 5, 6),
+            ('q1', 101,1), ('q2', 102,2), ('q3', 103,3), ('q4', 104,4), ('q5', 105,5),
+        ])
+        all_topologies.add_topology(factory.create_loop_topology(
+                topology_name, 
+                ext_mom={'q1':q1, 'q2': q2 , 'q3': q3, 'q4': q4, 'q5': q5}, 
+                mass_map={'p1': 0.0, 'p2': 0.0, 'p3': 0.0, 'p4': 0.0, 'p5': 0.0, 'p6': 0.0}, 
+                loop_momenta_names=('p4','p5'), # If not specified an arbitrary spanning tree will be used for momentum routing 
+                analytic_result=None
+             ),
+             entry_name = topology_name
+        )
+
+    return all_topologies
+
+if __name__=='__main__':
+
+    all_topologies = load()
+    all_topologies.export_to(os.path.join('.', 'deformation_paper_topologies.yaml'))
