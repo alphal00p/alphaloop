cf01576bc92f84f467c30e600bbccc218f6482e4
ValentinHirschi <valentin.hirschi@gmail.com>
2019-10-27 20:24:22 +0100

    Coded up all six 6-point 2-loop topologies of Fig.8 of Weinzierls paper in Deformation_paper_topologies.py

--- LTD/Deformation_paper_topologies.py
+++ LTD/Deformation_paper_topologies.py
@@ -2,6 +2,7 @@
 import os
 import vectors
 import math
+import sys
 
 import mpmath
 import numpy
@@ -61,9 +62,153 @@ def load(selected_topologies=None):
              entry_name = topology_name
         )
 
+    # Setting up Weinzierl's kinematic for all six 6-point 2-loop topologies of Fig.8 of ref. https://arxiv.org/pdf/1211.0509.pdf
+    rescaling = 1.0e-2
+    q1 = vectors.LorentzVector(
+        [-12.0588,1.00017,2.55373,-2.65288]
+    )*rescaling 
+    q2 = vectors.LorentzVector(
+        [-18.6089,8.9195,-6.43508,-9.61832]
+    )*rescaling
+    q3 = vectors.LorentzVector(
+        [-13.8389,4.73227,6.55009,1.55854]
+    )*rescaling
+    q4 = vectors.LorentzVector(
+        [-25.5377,-20.892,-4.32472,8.89684] 
+    )*rescaling
+    q6 = vectors.LorentzVector(
+        [90.0,0.0,0.0,0.0]            
+    )*rescaling
+    q5 = -q6-q4-q3-q2-q1
+
+    topology_name = "T2_6P_2L_Weinzierl_A"
+    if selected_topologies is None or topology_name in selected_topologies:
+        factory = TopologyGenerator([
+            ('p1', 1, 7), ('p2', 7, 8), ('p3', 8, 1),
+            ('p4', 7, 2), ('p5', 2, 3), ('p6', 3, 4), ('p7', 4, 5), ('p8', 5, 6), ('p9', 6, 8),
+            ('q1', 101,1), ('q2', 102,2), ('q3', 103,3), ('q4', 104,4), ('q5', 105,5), ('q6', 106,6)
+        ])
+        all_topologies.add_topology(factory.create_loop_topology(
+                topology_name, 
+                ext_mom={'q1':q1, 'q2': q2 , 'q3': q3, 'q4': q4, 'q5': q5, 'q6':q6}, 
+                mass_map={'p1': 0.0, 'p2': 0.0, 'p3': 0.0, 'p4': 0.0, 'p5': 0.0, 'p6': 0.0, 'p7': 0.0, 'p8': 0.0, 'p9': 0.0}, 
+                loop_momenta_names=('p1','p4'), 
+                analytic_result=complex(-8.66e-1,0.0j)
+             ),
+             entry_name = topology_name
+        )
+
+    topology_name = "T2_6P_2L_Weinzierl_B"
+    if selected_topologies is None or topology_name in selected_topologies:
+        factory = TopologyGenerator([
+            ('p1', 2, 7), ('p2', 7, 8), ('p3', 8, 1), ('p4', 1, 2),
+            ('p5', 7, 3), ('p6', 3, 4), ('p7', 4, 5), ('p8', 5, 6), ('p9', 6, 8), 
+            ('q1', 101,1), ('q2', 102,2), ('q3', 103,3), ('q4', 104,4), ('q5', 105,5), ('q6', 106,6)
+        ])
+        all_topologies.add_topology(factory.create_loop_topology(
+                topology_name, 
+                ext_mom={'q1':q1, 'q2': q2 , 'q3': q3, 'q4': q4, 'q5': q5, 'q6':q6}, 
+                mass_map={'p1': 0.0, 'p2': 0.0, 'p3': 0.0, 'p4': 0.0, 'p5': 0.0, 'p6': 0.0, 'p7': 0.0, 'p8': 0.0, 'p9': 0.0}, 
+                loop_momenta_names=('p1','p5'), 
+                analytic_result=complex(-1.17,0.0j)
+             ),
+             entry_name = topology_name
+        )
+
+    topology_name = "T2_6P_2L_Weinzierl_C"
+    if selected_topologies is None or topology_name in selected_topologies:
+        factory = TopologyGenerator([
+            ('p1', 3, 7), ('p2', 7, 8), ('p3', 8, 1), ('p4', 1, 2), ('p5', 2, 3),
+            ('p6', 7, 4), ('p7', 4, 5), ('p8', 5, 6), ('p9', 6, 8),
+            ('q1', 101,1), ('q2', 102,2), ('q3', 103,3), ('q4', 104,4), ('q5', 105,5), ('q6', 106,6)
+        ])
+        all_topologies.add_topology(factory.create_loop_topology(
+                topology_name, 
+                ext_mom={'q1':q1, 'q2': q2 , 'q3': q3, 'q4': q4, 'q5': q5, 'q6':q6}, 
+                mass_map={'p1': 0.0, 'p2': 0.0, 'p3': 0.0, 'p4': 0.0, 'p5': 0.0, 'p6': 0.0, 'p7': 0.0, 'p8': 0.0, 'p9': 0.0}, 
+                loop_momenta_names=('p1','p6'), 
+                analytic_result=complex(-7.75,0.0j)
+             ),
+             entry_name = topology_name
+        )
+
+    topology_name = "T2_6P_2L_Weinzierl_D"
+    if selected_topologies is None or topology_name in selected_topologies:
+        factory = TopologyGenerator([
+            ('p1', 1, 7), ('p2', 7, 6), ('p3', 6, 8), ('p4', 8, 1),
+            ('p5', 7, 2), ('p6', 2, 3), ('p7', 3, 4), ('p8', 4, 5), ('p9', 5, 8),
+            ('q1', 101,1), ('q2', 102,2), ('q3', 103,3), ('q4', 104,4), ('q5', 105,5), ('q6', 106,6)
+        ])
+        all_topologies.add_topology(factory.create_loop_topology(
+                topology_name, 
+                ext_mom={'q1':q1, 'q2': q2 , 'q3': q3, 'q4': q4, 'q5': q5, 'q6':q6}, 
+                mass_map={'p1': 0.0, 'p2': 0.0, 'p3': 0.0, 'p4': 0.0, 'p5': 0.0, 'p6': 0.0, 'p7': 0.0, 'p8': 0.0, 'p9': 0.0}, 
+                loop_momenta_names=('p1','p5'), 
+                analytic_result=complex(-1.91e-1,0.0j)
+             ),
+             entry_name = topology_name
+        )
+
+    topology_name = "T2_6P_2L_Weinzierl_E"
+    if selected_topologies is None or topology_name in selected_topologies:
+        factory = TopologyGenerator([
+            ('p1', 1, 7), ('p2', 7, 6), ('p3', 6, 5), ('p4', 5, 8), ('p5', 8, 1),
+            ('p6', 7, 2), ('p7', 2, 3), ('p8', 3, 4), ('p9', 4, 8),
+            ('q1', 101,1), ('q2', 102,2), ('q3', 103,3), ('q4', 104,4), ('q5', 105,5), ('q6', 106,6)
+        ])
+        all_topologies.add_topology(factory.create_loop_topology(
+                topology_name, 
+                ext_mom={'q1':q1, 'q2': q2 , 'q3': q3, 'q4': q4, 'q5': q5, 'q6':q6}, 
+                mass_map={'p1': 0.0, 'p2': 0.0, 'p3': 0.0, 'p4': 0.0, 'p5': 0.0, 'p6': 0.0, 'p7': 0.0, 'p8': 0.0, 'p9': 0.0}, 
+                loop_momenta_names=('p1','p6'), 
+                analytic_result=complex(-4.64e-1,0.0j)
+             ),
+             entry_name = topology_name
+        )
+
+    topology_name = "T2_6P_2L_Weinzierl_F"
+    if selected_topologies is None or topology_name in selected_topologies:
+        factory = TopologyGenerator([
+            ('p1', 2, 7), ('p2', 7, 6), ('p3', 6, 5), ('p4', 5, 8), ('p5', 8, 1), ('p6', 1, 2),
+            ('p7', 7, 3), ('p8', 3, 4), ('p9', 4, 8),
+            ('q1', 101,1), ('q2', 102,2), ('q3', 103,3), ('q4', 104,4), ('q5', 105,5), ('q6', 106,6)
+        ])
+        all_topologies.add_topology(factory.create_loop_topology(
+                topology_name, 
+                ext_mom={'q1':q1, 'q2': q2 , 'q3': q3, 'q4': q4, 'q5': q5, 'q6':q6}, 
+                mass_map={'p1': 0.0, 'p2': 0.0, 'p3': 0.0, 'p4': 0.0, 'p5': 0.0, 'p6': 0.0, 'p7': 0.0, 'p8': 0.0, 'p9': 0.0}, 
+                loop_momenta_names=('p1','p7'), 
+                analytic_result=complex(-1.03,0.0j)
+             ),
+             entry_name = topology_name
+        )
+
     return all_topologies
 
 if __name__=='__main__':
+    
+    selected_topologies = sys.argv[1:]
 
-    all_topologies = load()
-    all_topologies.export_to(os.path.join('.', 'deformation_paper_topologies.yaml'))
+#    selected_topologies = [
+#        'T1_Pentabox_physical',
+#        'T2_6P_2L_Weinzierl_A',
+#        'T2_6P_2L_Weinzierl_B',
+#        'T2_6P_2L_Weinzierl_C',
+#        'T2_6P_2L_Weinzierl_D',
+#        'T2_6P_2L_Weinzierl_E',
+#        'T2_6P_2L_Weinzierl_F',        
+#    ]
+
+    if len(selected_topologies)==0:
+        print("Now processing all topologies...")
+        selected_topologies = None
+    else:
+        print("Now processing the following topologies: %s"%(', '.join(selected_topologies)))
+
+    all_topologies = load(selected_topologies)
+
+    # Write out topoloies one by one in separate yaml files given the time it
+    # takes to generate them and their size.
+    for topology_name, topology in all_topologies.items():
+        TopologyCollection({topology_name:topology}).export_to(os.path.join('.', '%s.yaml'%topology_name))
+
