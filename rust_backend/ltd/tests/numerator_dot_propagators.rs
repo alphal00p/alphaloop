d701263ea84992c9cba88af77d8d3b07a884914d
Andrea Pelloni <apelloni90@gmail.com>
2020-05-26 17:40:22 +0200

    - test can be run in f128 of f64  - removed 2L_v7  - added timing infos

--- rust_backend/ltd/tests/numerator_dot_propagators.rs
+++ rust_backend/ltd/tests/numerator_dot_propagators.rs
@@ -15,66 +15,81 @@ use num_traits::{NumCast, One, ToPrimitive, Zero};
 use rand::prelude::*;
 use vector::LorentzVector;
 
+type TestFloat = f128::f128;
+//type TestFloat = f64;
+
 fn cross_check<'a>(
     topo: &mut Topology,
     x: &'a [f64],
-    cache: &mut LTDCache<f128::f128>,
-) -> (
-    Complex<f128::f128>,
-    Complex<f128::f128>,
-    Complex<f128::f128>,
-) {
+    cache: &mut LTDCache<TestFloat>,
+) -> (Complex<TestFloat>, Complex<TestFloat>, Complex<TestFloat>) {
     // Parameterize
     let mut k = [LorentzVector::default(); MAX_LOOP];
-    let mut jac_para = f128::f128::one();
+    let mut jac_para = TestFloat::one();
     for i in 0..topo.n_loops {
         // set the loop index to i + 1 so that we can also shift k
-        let (l_space, jac) =
-            Topology::parameterize(&x[i * 3..(i + 1) * 3], topo.e_cm_squared, i, &topo.settings);
+        let (l_space, jac) = Topology::parameterize::<TestFloat>(
+            &x[i * 3..(i + 1) * 3],
+            topo.e_cm_squared,
+            i,
+            &topo.settings,
+        );
 
         // there could be some rounding here
         let rot = topo.rotation_matrix;
         k[i] = LorentzVector::from_args(
-            f128::f128::zero(),
-            <f128::f128 as NumCast>::from(rot[0][0]).unwrap() * l_space[0]
-                + <f128::f128 as NumCast>::from(rot[0][1]).unwrap() * l_space[1]
-                + <f128::f128 as NumCast>::from(rot[0][2]).unwrap() * l_space[2],
-            <f128::f128 as NumCast>::from(rot[1][0]).unwrap() * l_space[0]
-                + <f128::f128 as NumCast>::from(rot[1][1]).unwrap() * l_space[1]
-                + <f128::f128 as NumCast>::from(rot[1][2]).unwrap() * l_space[2],
-            <f128::f128 as NumCast>::from(rot[2][0]).unwrap() * l_space[0]
-                + <f128::f128 as NumCast>::from(rot[2][1]).unwrap() * l_space[1]
-                + <f128::f128 as NumCast>::from(rot[2][2]).unwrap() * l_space[2],
+            TestFloat::zero(),
+            <TestFloat as NumCast>::from(rot[0][0]).unwrap() * l_space[0]
+                + <TestFloat as NumCast>::from(rot[0][1]).unwrap() * l_space[1]
+                + <TestFloat as NumCast>::from(rot[0][2]).unwrap() * l_space[2],
+            <TestFloat as NumCast>::from(rot[1][0]).unwrap() * l_space[0]
+                + <TestFloat as NumCast>::from(rot[1][1]).unwrap() * l_space[1]
+                + <TestFloat as NumCast>::from(rot[1][2]).unwrap() * l_space[2],
+            <TestFloat as NumCast>::from(rot[2][0]).unwrap() * l_space[0]
+                + <TestFloat as NumCast>::from(rot[2][1]).unwrap() * l_space[1]
+                + <TestFloat as NumCast>::from(rot[2][2]).unwrap() * l_space[2],
         );
         jac_para *= jac;
     }
-    let mut k_def: ArrayVec<[LorentzVector<Complex<f128::f128>>; MAX_LOOP]> = (0..topo.n_loops)
-        .map(|i| k[i].map(|x| Complex::new(x, f128::f128::zero())))
+    let mut k_def: ArrayVec<[LorentzVector<Complex<TestFloat>>; MAX_LOOP]> = (0..topo.n_loops)
+        .map(|i| k[i].map(|x| Complex::new(x, TestFloat::zero())))
         .collect();
     topo.compute_complex_cut_energies(&k_def[..topo.n_loops], cache)
         .unwrap();
 
     // Compute using standard LTD
+    let topo2 = topo.clone();
+    let start = std::time::Instant::now();
     topo.settings.general.partial_fractioning_threshold = -1.0;
-    let result_ltd = topo
-        .clone()
+    let result_ltd = topo2
         .evaluate_all_dual_integrands(&mut k_def[..topo.n_loops], cache)
         .unwrap();
+    let dt_ltd = start.elapsed();
+    println!("\t#LTD in {:?}", dt_ltd);
 
     // Compute using partial fractioning
+    let mut start = std::time::Instant::now();
     topo.settings.general.partial_fractioning_threshold = 1e-99;
-    topo.numerator
-        .evaluate_reduced_in_lb(&k_def, 0, cache, 0, true, true);
-
     let result_pf1 = if topo.n_loops == 1 {
-        topo.clone()
+        let topo2 = topo.clone();
+        start = std::time::Instant::now();
+        topo2
+            .numerator
+            .evaluate_reduced_in_lb(&k_def, 0, cache, 0, true, true);
+
+        topo2
             .evaluate_all_dual_integrands(&mut k_def[..topo.n_loops], cache)
             .unwrap()
     } else {
         Complex::zero()
     };
+    let dt_pf1 = start.elapsed();
+    println!("\t#PF1 in {:?}", dt_pf1);
 
     // Compute using partial fractioning MultiLoop
+    let start = std::time::Instant::now();
+    topo.numerator
+        .evaluate_reduced_in_lb(&k_def, 0, cache, 0, true, true);
     for prop_pow in cache.propagator_powers.iter_mut() {
         *prop_pow = 0;
     }
@@ -89,6 +104,12 @@ fn cross_check<'a>(
         &topo.loop_lines,
         &topo.propagator_id_to_ll_id,
         cache,
+    );
+    let dt_pf2 = start.elapsed();
+    println!(
+        "\t#PF2 in {:?} :: PF2/LTD = {:?}",
+        dt_pf2,
+        dt_pf2.as_nanos() as f64 / dt_ltd.as_nanos() as f64
     );
     (result_ltd, result_pf1, result_pf2)
 }
@@ -122,18 +143,18 @@ mod dot_propagators_1loop {
 
         // map the point back from momentum-space to the unit hypercube
         for (i, x) in pt.chunks_exact_mut(3).enumerate() {
-            let r = Topology::inv_parametrize::<f128::f128>(
+            let r = Topology::inv_parametrize::<TestFloat>(
                 &LorentzVector::from_args(0., x[0], x[1], x[2]).cast(),
                 topo.e_cm_squared,
                 i,
                 &topo.settings,
             );
-            x[0] = f128::f128::to_f64(&r.0[0]).unwrap();
-            x[1] = f128::f128::to_f64(&r.0[1]).unwrap();
-            x[2] = f128::f128::to_f64(&r.0[2]).unwrap();
+            x[0] = TestFloat::to_f64(&r.0[0]).unwrap();
+            x[1] = TestFloat::to_f64(&r.0[1]).unwrap();
+            x[2] = TestFloat::to_f64(&r.0[2]).unwrap();
         }
         // Compute values using e-fractioning the standard ltd sum over cuts
-        let mut cache = LTDCache::<f128::f128>::new(&topo);
+        let mut cache = LTDCache::<TestFloat>::new(&topo);
         let (result_ltd, result_pf1, result_pf2) = cross_check(&mut topo, pt, &mut cache);
 
         println!(
@@ -182,18 +203,18 @@ mod dot_propagators_1loop {
 
         // map the point back from momentum-space to the unit hypercube
         for (i, x) in pt.chunks_exact_mut(3).enumerate() {
-            let r = Topology::inv_parametrize::<f128::f128>(
+            let r = Topology::inv_parametrize::<TestFloat>(
                 &LorentzVector::from_args(0., x[0], x[1], x[2]).cast(),
                 topo.e_cm_squared,
                 i,
                 &topo.settings,
             );
-            x[0] = f128::f128::to_f64(&r.0[0]).unwrap();
-            x[1] = f128::f128::to_f64(&r.0[1]).unwrap();
-            x[2] = f128::f128::to_f64(&r.0[2]).unwrap();
+            x[0] = TestFloat::to_f64(&r.0[0]).unwrap();
+            x[1] = TestFloat::to_f64(&r.0[1]).unwrap();
+            x[2] = TestFloat::to_f64(&r.0[2]).unwrap();
         }
         // Compute values using e-fractioning the standard ltd sum over cuts
-        let mut cache = LTDCache::<f128::f128>::new(&topo);
+        let mut cache = LTDCache::<TestFloat>::new(&topo);
         let (result_ltd, result_pf1, result_pf2) = cross_check(&mut topo, pt, &mut cache);
 
         println!(
@@ -435,18 +456,18 @@ mod dot_propagators_2loops {
 
         // map the point back from momentum-space to the unit hypercube
         for (i, x) in pt.chunks_exact_mut(3).enumerate() {
-            let r = Topology::inv_parametrize::<f128::f128>(
+            let r = Topology::inv_parametrize::<TestFloat>(
                 &LorentzVector::from_args(0., x[0], x[1], x[2]).cast(),
                 topo.e_cm_squared,
                 i,
                 &topo.settings,
             );
-            x[0] = f128::f128::to_f64(&r.0[0]).unwrap();
-            x[1] = f128::f128::to_f64(&r.0[1]).unwrap();
-            x[2] = f128::f128::to_f64(&r.0[2]).unwrap();
+            x[0] = TestFloat::to_f64(&r.0[0]).unwrap();
+            x[1] = TestFloat::to_f64(&r.0[1]).unwrap();
+            x[2] = TestFloat::to_f64(&r.0[2]).unwrap();
         }
         // Compute values using e-fractioning the standard ltd sum over cuts
-        let mut cache = LTDCache::<f128::f128>::new(&topo);
+        let mut cache = LTDCache::<TestFloat>::new(&topo);
         let (result_ltd, result_pf1, result_pf2) = cross_check(&mut topo, pt, &mut cache);
         println!("Energies: {:?}", cache.complex_cut_energies);
         println!("ltd = {:?}\npf2 = {:?}", result_ltd, result_pf2);
@@ -490,18 +511,18 @@ mod dot_propagators_2loops {
 
         // map the point back from momentum-space to the unit hypercube
         for (i, x) in pt.chunks_exact_mut(3).enumerate() {
-            let r = Topology::inv_parametrize::<f128::f128>(
+            let r = Topology::inv_parametrize::<TestFloat>(
                 &LorentzVector::from_args(0., x[0], x[1], x[2]).cast(),
                 topo.e_cm_squared,
                 i,
                 &topo.settings,
             );
-            x[0] = f128::f128::to_f64(&r.0[0]).unwrap();
-            x[1] = f128::f128::to_f64(&r.0[1]).unwrap();
-            x[2] = f128::f128::to_f64(&r.0[2]).unwrap();
+            x[0] = TestFloat::to_f64(&r.0[0]).unwrap();
+            x[1] = TestFloat::to_f64(&r.0[1]).unwrap();
+            x[2] = TestFloat::to_f64(&r.0[2]).unwrap();
         }
         // Compute values using e-fractioning the standard ltd sum over cuts
-        let mut cache = LTDCache::<f128::f128>::new(&topo);
+        let mut cache = LTDCache::<TestFloat>::new(&topo);
         let (result_ltd, result_pf1, result_pf2) = cross_check(&mut topo, pt, &mut cache);
         println!("Energies: {:?}", cache.complex_cut_energies);
         println!("ltd = {:?}\npf2 = {:?}", result_ltd, result_pf2);
@@ -1046,7 +1067,6 @@ mod dot_propagators_multiloops {
     /// |2L_v4 -> TriangleBox_physical
     /// |2L_v5 -> manual_6pt_Weinzierl_a
     /// |2L_v6 -> manual_AltDoubleTriangle
-    /// |2L_v7 -> manual_DoubleTriangle_no_ellipse_ala_weinzierl
     /// |3L_v1 -> PRL_Mercedes
     /// |3L_v2 -> TriangleBoxTriangle
     /// |3L_v3 -> manual_Mercedes
@@ -1072,7 +1092,6 @@ mod dot_propagators_multiloops {
     /// |      |    4    |      3     |   2\|1\|3
     /// |      |    5    |      3     |   2\|6\|1
     /// |      |    6    |      3     |   2\|2\|1
-    /// |      |    7    |      3     |   2\|2\|1
     /// |  3   |    1    |      6     |   2\|1\|1\|1\|1\|1
     /// |      |    2    |      5     |   2\|2\|2\|1\|1
     /// |      |    3    |      6     |   1\|1\|2\|1\|1\|1
@@ -1095,22 +1114,23 @@ mod dot_propagators_multiloops {
         for coeff in coeffs.iter() {
             topo.numerator_tensor_coefficients.push(*coeff);
         }
+        //topo.numerator_tensor_coefficients.push((1.0, 0.0));
         topo.process(true);
 
         // map the point back from momentum-space to the unit hypercube
         for (i, x) in pt.chunks_exact_mut(3).enumerate() {
-            let r = Topology::inv_parametrize::<f128::f128>(
+            let r = Topology::inv_parametrize::<TestFloat>(
                 &LorentzVector::from_args(0., x[0], x[1], x[2]).cast(),
                 topo.e_cm_squared,
                 i,
                 &topo.settings,
             );
-            x[0] = f128::f128::to_f64(&r.0[0]).unwrap();
-            x[1] = f128::f128::to_f64(&r.0[1]).unwrap();
-            x[2] = f128::f128::to_f64(&r.0[2]).unwrap();
+            x[0] = TestFloat::to_f64(&r.0[0]).unwrap();
+            x[1] = TestFloat::to_f64(&r.0[1]).unwrap();
+            x[2] = TestFloat::to_f64(&r.0[2]).unwrap();
         }
         // Compute values using e-fractioning the standard ltd sum over cuts
-        let mut cache = LTDCache::<f128::f128>::new(&topo);
+        let mut cache = LTDCache::<TestFloat>::new(&topo);
         let (result_ltd, result_pf1, result_pf2) = cross_check(&mut topo, pt, &mut cache);
 
         if topo.n_loops == 1 {
@@ -1238,18 +1258,6 @@ mod dot_propagators_multiloops {
             .collect();
         // Test a two loop topology with two propagators per loop line
         test_multiloops("2L_v6", &mut pt, &coeffs);
-    }
-
-    #[allow(non_snake_case)]
-    #[test]
-    fn MultiLoop_2L_v7_const_dense() {
-        let mut rng = rand::thread_rng();
-        // Select point in momentum space: 3*n_loops
-        let mut pt: Vec<f64> = (0..3 * 2).map(|_| 1.0 - 2.0 * rng.gen::<f64>()).collect();
-        // Create the random coefficients needed for the rank 4 test
-        let coeffs: Vec<(f64, f64)> = vec![(1.0, 0.0)];
-        // Test a two loop topology with two propagators per loop line
-        test_multiloops("2L_v7", &mut pt, &coeffs);
     }
 
     #[allow(non_snake_case)]
