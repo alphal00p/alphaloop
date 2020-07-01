d242a81047bc039c19be6a99c7aca0a2799ac331
Andrea Pelloni <apelloni90@gmail.com>
2020-07-01 13:00:44 +0200

    validation more automatic then ever... now it also generates the output from aL@MG5

--- validation/validate.py
+++ validation/validate.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3.8
 """
 Run Validation for different processes
 """
 
 
-import getopt
 import glob
 import os
 import subprocess
 import sys
+from argparse import ArgumentParser
 from pathlib import Path
 
 import pandas as pd
@@ -31,7 +31,7 @@ Path(VALIDATION_PATH).mkdir(parents=True, exist_ok=True)
 rALPHA = pjoin(AL_PATH, 'rust_backend', 'target', 'debug', 'ltd')
 
 
-def set_hyperparameters(process_name, sg_name, multi_channeling=False, workspace=None, multi_settings={}):
+def set_hyperparameters(process_name, sg_name, multi_channeling=False, workspace=None, no_jets=False, multi_settings={}):
     '''Create the correct hyperparameter file'''
     path = pjoin(AL_PATH, 'LTD', 'hyperparameters.yaml')
     hyperparameters = yaml.load(open(path, 'r'), Loader=yaml.Loader)
@@ -41,6 +41,8 @@ def set_hyperparameters(process_name, sg_name, multi_channeling=False, workspace
     except KeyError:
         min_jpt = hyperparameters['Selectors']['jet']['min_jpt']
 
+    if no_jets:
+        min_jpt = 'nocut'
     # Set some default values for the hyperparameter
     # Set values for General
     hyperparameters['General']['multi_channeling'] = multi_channeling
@@ -49,16 +51,17 @@ def set_hyperparameters(process_name, sg_name, multi_channeling=False, workspace
     hyperparameters['General']['topology'] = "%s_%s" % (sg_name, min_jpt)
     hyperparameters['General']['res_file_prefix'] = "%s_" % process_name
     hyperparameters['General']['log_file_prefix'] =\
-        "stats/%s_%s_%d_" % (process_name, sg_name, min_jpt)
+        "stats/%s_%s_%s_" % (process_name, sg_name, min_jpt)
     hyperparameters['General']['partial_fractioning_multiloop'] = True
     hyperparameters['General']['partial_fractioning_threshold'] = -1
     # Set values for Integrator
     hyperparameters['Integrator']['dashboard'] = False
     hyperparameters['Integrator']['integrator'] = 'vegas'
-    # Set values for CrossSeciton
-    print(hyperparameters['Selectors'].keys())
+    hyperparameters['Integrator']['internal_parallelization'] = True
+    hyperparameters['Integrator']['n_vec'] = int(1e8)
+    # Set values for CrossSection
     hyperparameters['CrossSection']['numerator_source'] = 'FORM'
-    hyperparameters['CrossSection']['picobarn'] = True
+    hyperparameters['CrossSection']['picobarns'] = True
 
     # General settings can be parsed though the multi_settings valiable
     # It must have the same structure as hyperparameters
@@ -76,19 +79,20 @@ def set_hyperparameters(process_name, sg_name, multi_channeling=False, workspace
 
     # Create the hyperpamater file
     output_path = pjoin(workspace, 'hyperparameters',
-                        '%s_%s_%d.yaml' % (process_name, sg_name, min_jpt))
+                        '%s_%s_%s.yaml' % (process_name, sg_name, min_jpt))
     with open(output_path, "w") as stream:
         yaml.dump(hyperparameters, stream)  # default_flow_style=None)
     return output_path
 
 
-def run_super_graph(process_name, sg_name, aL_path_output, suffix='', multi_settings={}, workspace=None):
+def run_super_graph(process_name, sg_name, aL_path_output, suffix='', multi_settings={}, no_jets=False, workspace=None):
     ''' run supergraph for specific process '''
     # Create hyperparameter file
     hyperparameters_path = set_hyperparameters(
         process_name,
         sg_name,
         workspace=workspace,
+        no_jets=no_jets,
         multi_settings=multi_settings)
     print(hyperparameters_path)
     # run integration
@@ -99,17 +103,21 @@ def run_super_graph(process_name, sg_name, aL_path_output, suffix='', multi_sett
     rust_executable += ['-c', '%d' % CORES]
 
     # Store output
-    #err_path = pjoin(WORKSPACE,'%s.err'%sg_name)
-    #out_path = pjoin(WORKSPACE,'%s.out'%sg_name)
+    err_path = pjoin(WORKSPACE, '%s.err' % sg_name)
+    out_path = pjoin(WORKSPACE, '%s.out' % sg_name)
 
     r = subprocess.run(rust_executable,
                        cwd=workspace,
+                       stdout=open(out_path, 'w'),
+                       stderr=open(err_path, 'w'),
                        env=os.environ.update(
                            {'MG_NUMERATOR_PATH': '%s/' % aL_path_output}),
-                       capture_output=True)
+                       capture_output=False)
     if r.returncode != 0:
-        print(r.stdout.decode('UTF-8'))
-        print(r.stderr.decode('UTF-8'))
+        print("\033[1;31mFAIL: see {} and {} for further details.\033[0m".format(
+            err_path, out_path))
+    #    print(r.stdout.decode('UTF-8'))
+    #    print(r.stderr.decode('UTF-8'))
 
 
 def get_result(filename):
@@ -132,7 +140,6 @@ def get_result(filename):
 
 if __name__ == "__main__":
     # For the moment run for LO
-    suffix = 'LO'
 
     # Extra settings
     hyper_settings = {'General': {},
@@ -144,47 +151,77 @@ if __name__ == "__main__":
                       'Parameterization': {},
                       }
 
+    # Parse arguments
+    parser = ArgumentParser()
+    parser.add_argument("--process", dest="process", help="process name")
+    parser.add_argument("-g", "--generate", action="store_true", dest="generate", default=False,
+                        help="Generate alphaLoop output")
+    parser.add_argument("-r", "--run", action="store_true", dest="run", default=False,
+                        help="run the integration for all SG")
+    parser.add_argument("-c", "--collect", action="store_true", dest="collect", default=False,
+                        help="Collect the reults from individual SQ in one CSV file")
+    parser.add_argument("-v", "--validate", action="store_true", dest="validate", default=False,
+                        help="Compare with bench results")
+    parser.add_argument("--no_jets", action="store_true", dest="no_jets", default=False,
+                        help="No jets in the final state")
+    parser.add_argument("--min_jpt",  dest="min_jpt", default=100, type=int,
+                        help="Jet cutoff")
+    parser.add_argument("--n_max", dest="n_max", default=int(1e9), type=int,
+                        help="max number of evaluation with Vegas")
+    parser.add_argument("--cores", dest="cores", default=CORES, type=int,
+                        help="number of cores used during integration")
+    parser.add_argument("-@", dest="order", default='LO', type=str,
+                        help="Perturabtive QCD order")
+    args = parser.parse_args()
+    if args.process is None:
+        raise ValueError(
+            "Missing process definition: %s --process=epem_a_ttx --run" % sys.argv[0])
+    if not any(getattr(args, k) for k in ['run', 'collect', 'generate', 'validate']):
+        raise ValueError(
+            "\nNeed to specify at leas one of --run, --collect, --generate and --validate\n")
+
+    process_name = args.process
+    suffix = args.order
+    CORES = args.cores
     # Integrator Settings
-    hyper_settings['Integrator']['n_max'] = int(1e9)
+    hyper_settings['Integrator']['n_max'] = args.n_max
     hyper_settings['Integrator']['n_new'] = int(1e5)
     hyper_settings['Integrator']['n_start'] = int(1e5)
     hyper_settings['Integrator']['n_increase'] = int(1e5)
     # Selector Settings
-    hyper_settings['Selectors']['active_selectors'] = ['jet']
-    hyper_settings['Selectors']['jet'] = {'min_jets': 1, 'min_jpt': 10}
-
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hp:', [
-                                   'run', 'collect', 'process='])
-    except getopt.GetoptError:
-        print("\n>>> Try something like: %s --process=epem_a_ttx --run\n" %
-              sys.argv[0])
-        raise
-    opts = {o: v for o, v in opts}
-    # Parse arguments
-    if opts.get('--process') is None:
-        raise getopt.GetoptError(
-            "\nMissing process definition: %s --process=epem_a_ttx run\n" % sys.argv[0])
-    if opts.get('--run') is None and opts.get('--collect') is None:
-        raise getopt.GetoptError("\nNeed to specify --run or --collect\n")
-    process_name = opts['--process']
-
+    hyper_settings['Selectors']['active_selectors'] = [
+    ] if args.no_jets else ['jet']
+    hyper_settings['Selectors']['jet'] = {
+        'min_jets': 1,
+#        'dR': 0.4,
+        'min_jpt': args.min_jpt}
     # define alpha loop input folder
-    aL_process_output = pjoin(MG_PATH, 'TEST_QGRAF_%s' % process_name)
+    aL_process_output = pjoin(MG_PATH, 'TEST_QGRAF_%s_%s' %
+                              (process_name, suffix))
+    # Define workspace for the computation of all the diagrams
+    WORKSPACE = pjoin(VALIDATION_PATH, '%s_%s' % (process_name, suffix))
+
+    #############
+    #  Generate
+    #############
+    if args.generate:
+        MG5 = pjoin(MG_PATH, 'bin', 'mg5_aMC')
+        CARD = pjoin(VALIDATION_PATH, 'cards', "%s_%s.aL" %
+                     (process_name, suffix))
+
+        r = subprocess.run([MG5, '--mode=alphaloop', CARD],
+                           cwd=MG_PATH,
+                           capture_output=False)
 
     # load instructions
     instructions = yaml.load(
         open(pjoin(aL_process_output, 'Rust_inputs',
                    'all_QG_supergraphs.yaml'), 'r'),
         Loader=yaml.Loader)
-
-    # Define workspace for the computation of all the diagrams
-    WORKSPACE = pjoin(VALIDATION_PATH, '%s_%s' % (process_name, suffix))
-
     #############
     #   RUN
     #############
-    if opts.get('--run') is not None:
+    if args.run:
         # Create workspace
         Path(WORKSPACE).mkdir(parents=True, exist_ok=True)
         Path(pjoin(WORKSPACE, 'stats')).mkdir(parents=True, exist_ok=True)
@@ -196,24 +233,19 @@ if __name__ == "__main__":
                             sg_name,
                             aL_process_output, suffix='_%s' % suffix,
                             multi_settings=hyper_settings,
+                            no_jets=args.no_jets,
                             workspace=WORKSPACE)
     #############
     #  COLLECT
     #############
-    if opts.get('--collect') is not None:
+    if args.collect:
         sg_list = [topo['name'] for topo in instructions['topologies']]
-        output_file = pjoin(WORKSPACE, "results.csv")
+        output_file = pjoin(WORKSPACE, "%s_%s_results.csv" %
+                            (process_name, args.min_jpt))
         results = []
-        print(output_file)
         for sg in instructions['topologies']:
-            print("%s/*%s_%s*.dat" % (
-                WORKSPACE,
-                sg['name'],
-                hyper_settings['Selectors']['jet']['min_jpt']))
-            filename = glob.glob("%s/*%s_%s*.dat" % (
-                WORKSPACE,
-                sg['name'],
-                hyper_settings['Selectors']['jet']['min_jpt']))[0]
+            filename = glob.glob("%s/*%s_%s*.dat" %
+                                 (WORKSPACE, sg['name'], 'nocut' if args.no_jets else args.min_jpt))[0]
             print("Extracting %s: %s" % (sg['name'], filename), end="\n")
 
             data = [sg['name'], sg['multiplicity']]
@@ -223,3 +255,25 @@ if __name__ == "__main__":
         df = pd.DataFrame(results, columns=[
             "id", "multiplicity", "neval", "real", "real_err", "imag", "imag_err"])
         df.to_csv(output_file, sep=',', encoding='utf-8', index=False)
+
+    #############
+    #  VALIDATE
+    #############
+    if args.validate:
+        # Bench result
+        bench_results = pd.read_csv(
+            pjoin(VALIDATION_PATH, 'bench', "%s_epem.csv" % suffix))
+        # Filter by process
+        bench_results = bench_results[bench_results['Process'] == process_name]
+        mycut = 'nocut' if args.no_jets else str(args.min_jpt)
+        # Filter by min_jpt
+        bench_results = bench_results[ bench_results['min_ptj[GeV]'] == mycut]
+        print("\033[1mBENCH:\033[0m\n",bench_results)
+
+        # alphaLoop result
+        aL_results = pd.read_csv(pjoin(WORKSPACE, "%s_%s_results.csv" %
+                            (process_name, args.min_jpt)))
+        print("\033[1maL SGs:\033[0m\n", aL_results)
+        aL_total = aL_results[['real', 'real_err', 'imag','imag_err']].multiply(aL_results['multiplicity'],axis='index')
+        print("\033[1maL Total:\033[0m\n", aL_total.sum())
+
