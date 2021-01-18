4d71e666f28bdf062cb57fdd2ce99e868096d812
Ben Ruijl <benruyl@gmail.com>
2021-01-18 15:28:38 +0100

    Updated Cuba to 4.2.1 in deploy and build script

--- rust_backend/build.rs
+++ rust_backend/build.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("cargo:rustc-link-search=../libraries/fjcore");
     println!("cargo:rustc-link-search=../libraries/ecos");
     println!("cargo:rustc-link-search=../libraries/scs/out");
-    println!("cargo:rustc-link-search=../libraries/Cuba-4.2");
+    println!("cargo:rustc-link-search=../libraries/Cuba-4.2.1");
     println!("cargo:rustc-link-lib=stdc++");
     #[cfg(target_os = "macos")]
     println!("cargo:rustc-link-lib=gcc");
