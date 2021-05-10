83d2002bd6f9ba49f0a5a59aef3e3db52205b735
armin schweitzer <armin.schweitzer@phys.ethz.ch>
2021-05-10 16:36:32 +0200

    added WW for dario

---
+++ LTD/amplitudes/dd2W/run_qqbarWW.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+import yaml
+
+pjoin = os.path.join
+
+
+# # Define path to runcard
+# runcard_path = "./runcard_qqbar2a.yaml"
+
+# # Get from runcard the location of the alphaloop folder
+# with open(runcard_path, "r") as f:
+#     runcard = yaml.load(f, Loader=yaml.Loader)
+# alphaloop_dir = runcard['directories']['alphaloop']
+
+# if True:
+#     sys.path.append(pjoin(alphaloop_dir, 'LTD/'))
+#     from amplitudes_rewrite.amp_exporter import AmpExporter
+
+
+# os.system('rm -rf qqbar2a ')
+# exporter = AmpExporter.from_runcard(runcard_path)
+# exporter.export()
+
+
+
+# Define path to runcard
+runcard_path = "./WW_runcard.json"
+
+# Get from runcard the location of the alphaloop folder
+with open(runcard_path, "r") as f:
+    runcard = yaml.load(f, Loader=yaml.Loader)
+alphaloop_dir = runcard['directories']['alphaloop']
+
+if True:
+    sys.path.append(pjoin(alphaloop_dir, 'LTD/'))
+    from amplitudes_rewrite.amp_exporter import AmpExporter
+
+
+os.system('rm -rf WW ')
+exporter = AmpExporter.from_runcard(runcard_path)
+exporter.export()
