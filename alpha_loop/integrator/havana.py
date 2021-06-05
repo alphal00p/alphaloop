5b1fef0e87edd2b9b2d58a11500033a6b5663a7a
ValentinHirschi <valentin.hirschi@gmail.com>
2021-06-05 17:17:02 +0200

    Estheatical improvements.

--- alpha_loop/integrator/havana.py
+++ alpha_loop/integrator/havana.py
@@ -290,7 +290,7 @@ class HavanaMockUp(object):
         res = ['Distribution of %d SGs:'%len(sorted_bins)]
         for bins_chunk in [sorted_bins[i_chunk:i_chunk+5] for i_chunk in range(0,len(sorted_bins),5)]:
             res.append('  '+' | '.join( '%-40s'%(
-                '%s(I=%.5g+/-(%.2g%%),p=%.2f%%)'%(
+                '%s(I=%.5g(%.2g%%),p=%.2f%%)'%(
                     bin['SG_name'], bin['integral_estimate'], 
                     abs(bin['error_estimate']/bin['integral_estimate'])*100. if bin['integral_estimate']!=0. else 0.,
                     bin['p']*100.
@@ -304,7 +304,7 @@ class HavanaMockUp(object):
                     sorted_channel_bins = sorted([(i_c, b) for i_c, b in enumerate(SG_bin['channel_grid'])], key=lambda b: b[1]['error_estimate'] if sort_by_variance else abs(b[1]['integral_estimate']), reverse=True)
                     for bins_chunk in [sorted_channel_bins[i_chunk:i_chunk+5] for i_chunk in range(0,len(sorted_channel_bins),5)]:
                         res.append('  '+' | '.join( '%-40s'%(
-                            'C%d(I=%.5g+/-(%.2g%%),p=%.2f%%)'%(
+                            'C%d(I=%.5g(%.2g%%),p=%.2f%%)'%(
                                 i_c, bin['integral_estimate'], 
                                 abs(bin['error_estimate']/bin['integral_estimate'])*100. if bin['integral_estimate']!=0. else 0.,
                                 bin['p']*100.
@@ -609,7 +609,9 @@ class HavanaIntegrator(integrators.VirtualIntegrator):
                 else:
                     # Scattering data is not really necessary as I believe this is the right thing to do only for data common to multiple jobs. 
                     # But I don't like the warning when not doing so, so I'll put it.
+                    if self._DEBUG: logger.info("Scattering sampling input for job #%d..."%job_ID)
                     this_scattered_sample = client.scatter(this_sample)
+                    if self._DEBUG: logger.info("Done with scattering sampling input.")
 
                 if self._DEBUG: logger.info("Submitting job %d."%job_ID)
                 futures.append(client.submit(HavanaIntegrandWrapper, 
@@ -633,18 +635,20 @@ class HavanaIntegrator(integrators.VirtualIntegrator):
 
             try:
 
+                if self._DEBUG: logger.info("Now waiting for a job to complete...")
                 for completed_future in as_completed(futures):
                     
                     t1 = time.time()
                     if self._DEBUG: logger.info("Now receiving result from a job...")
                     job_ID, run_time, results = completed_future.result()
-                    if self._DEBUG: logger.info("Finished receiving result job %d..."%job_ID)
                     if self.pickle_IO:
                         if self._DEBUG: logger.info("Loading pickled results for job %d..."%job_ID)
                         results_pickle_path = results
                         results = pickle.load( open( results_pickle_path, 'rb' ) )
                         os.remove(results_pickle_path)
-                        if self._DEBUG: logger.info("Finished loading pickled results.")
+                        if self._DEBUG: logger.info("Finished loading pickled results of %d samples."%len(results))
+                    else:
+                        if self._DEBUG: logger.info("Finished receiving %d sample result from job %d..."%(len(results),job_ID))
 
                     cumulative_IO_time += time.time()-t1
                     cumulative_job_time += run_time
@@ -703,7 +707,11 @@ class HavanaIntegrator(integrators.VirtualIntegrator):
             current_n_points += current_step
             current_step += self.n_increase
             current_iteration += 1
-        
+
+            # Keep printout of last step of this iteration.
+            self.canvas = None
+            logger.info("Now on iteration #%d"%current_iteration)
+
         return self.havana.get_current_estimate()
 
     def update_status( self,
@@ -716,8 +724,8 @@ class HavanaIntegrator(integrators.VirtualIntegrator):
 
         # Update the run stat line and monitoring canvas
         monitoring_report = []
-        monitoring_report.append( '| Jobs: completed = %d and submitted = %d/%d on %d workers.\n| Total n_pts: %.1fM ( %s ms / pt on one core, %s pts / s overall ). n_pts for this iteration #%d: %.1fM/%.1fM (%.1f%%).'%(
-            n_jobs_total_completed, n_submitted-n_done, n_submitted, self.n_workers,
+        monitoring_report.append( '| Jobs: completed = %d (avg %s) and running = %d/%d on %d workers.\n| Total n_pts: %.1fM ( %s ms / pt on one core, %s pts / s overall ). n_pts for this iteration #%d: %.1fM/%.1fM (%.1f%%).'%(
+            n_jobs_total_completed, '%.3g min/job'%((cumulative_job_time/n_jobs_total_completed)/60.) if n_jobs_total_completed>0 else 'N/A', n_submitted-n_done, n_submitted, self.n_workers,
             n_tot_points/(1.e6), '%.3g'%((curr_integration_time/n_tot_points)*self.n_workers*1000.) if n_tot_points>0 else 'N/A', 
             ('%.1fK'%(n_tot_points/curr_integration_time/1000.) if n_tot_points>0 else 'N/A'),
             current_iteration_number,
