b2004f16654249c1f825d494e8930d55d1d8e2db
ValentinHirschi <valentin.hirschi@gmail.com>
2022-03-31 08:42:36 +0200

    Add automatic recycling of existing result if file out_*.proto_c already exists for a given supergraph.

--- alpha_loop/FORM_processing.py
+++ alpha_loop/FORM_processing.py
@@ -2199,6 +2199,12 @@ class FORMSuperGraphIsomorphicList(list):
                 [ FORM_source, ]
         )
 
+        if pjoin(selected_workspace,'FORM_run_cmd_%d.exe'%i_graph) and \
+            ( (FORM_vars['INTEGRAND'] not in ['PF','both']) or os.path.isfile(pjoin(selected_workspace,'out_integrand_PF_%d.proto_c'%i_graph)) ) and \
+            ( (FORM_vars['INTEGRAND'] not in ['LTD','both']) or os.path.isfile(pjoin(selected_workspace,'out_integrand_LTD_%d.proto_c'%i_graph)) ):
+            logger.info("Recycling already existing FORM output for graph #%d. Remove file out_integrand_*_%d.proto_c to force a reprocessing."%(i_graph, i_graph))
+            return True
+
         with open(pjoin(selected_workspace,'FORM_run_cmd_%d.exe'%i_graph), 'w') as f:
             f.write(FORM_cmd)
 
