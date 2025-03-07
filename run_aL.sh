092a2916c1137c7596e90ebffe2cfd1b9ca7ddf5
Valentin Hirschi <valentin.hirschi@gmail.com>
2025-03-07 18:04:06 +0000

    updating graphs with scored LMBs

--- run_aL.sh
+++ run_aL.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=/home/vhirschi/aa_ttx/MG5_aMC_v3_5_7/PLUGIN/new_aL_deformation/libraries/scs/out/libscsdir.so python3 ./bin/mg5_aMC --mode new_aL_deformation $1
+LD_PRELOAD=/home/vhirshi/alphaloop/MG5_aMC_v3_5_7/PLUGIN/new_aL_deformation/libraries/scs/out/libscsdir.so python3 ./bin/mg5_aMC --mode new_aL_deformation $1
